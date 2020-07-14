16d6d9d552fa64c3d2c33298ae157edafe84aba3
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2020-07-14 16:47:13 -0500

    deleted unused FootprintEstimatorFactory file | dan/jose

--- src/domain/FootprintEstimatorFactory.ts
+++
@@ -1,8 +0,0 @@
-import FootprintEstimator from './FootprintEstimator'
-import { StorageEstimator } from './StorageEstimator'
-
-export class FootprintEstimatorFactory {
-  static create(): FootprintEstimator {
-    return new StorageEstimator()
-  }
-}
