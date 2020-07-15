781045ee88f75d1cfef659dfe2ca070a88d0f5a5
austin-garrard <austin.w.garrard@gmail.com>
2020-07-15 10:48:09 -0500

    add s3 columns to table output | Austin, Megan

--- src/application/EmissionsTable.ts
+++ src/application/EmissionsTable.ts
@@ -13,14 +13,18 @@ const initialTotals = () => ({
     wattHours: 0,
     co2e: 0,
   },
+  s3: {
+    wattHours: 0,
+    co2e: 0,
+  },
 })
 
 const displayWattHours = (wattHours: number) => `${wattHours.toFixed(2)} Watts`
 const displayCo2e = (co2e: number) => `${co2e.toFixed(6)} Kg CO2e`
 
 export default function EmissionsTable(estimations: EstimationResult[]): string {
-  const table: string[][] = [['Date (UTC)', 'EBS Wattage', 'EBS CO2e Emissions']]
-  const colWidths: number[] = [20, 20, 30]
+  const table: string[][] = [['Date (UTC)', 'EBS Wattage', 'EBS CO2e Emissions', 'S3 Wattage', 'S3 CO2e Emissions']]
+  const colWidths: number[] = [20, 20, 30, 20, 30]
 
   const grandTotals: Totals = initialTotals()
 
@@ -38,10 +42,18 @@ export default function EmissionsTable(estimations: EstimationResult[]): string 
       moment(estimationResult.timestamp).utc().format('YYYY-MM-DD'),
       displayWattHours(subTotals['ebs'].wattHours),
       displayCo2e(subTotals['ebs'].co2e),
+      displayWattHours(subTotals['s3'].wattHours),
+      displayCo2e(subTotals['s3'].co2e),
     ])
   })
 
-  table.push(['Total', displayWattHours(grandTotals['ebs'].wattHours), displayCo2e(grandTotals['ebs'].co2e)])
+  table.push([
+    'Total',
+    displayWattHours(grandTotals['ebs'].wattHours),
+    displayCo2e(grandTotals['ebs'].co2e),
+    displayWattHours(grandTotals['s3'].wattHours),
+    displayCo2e(grandTotals['s3'].co2e),
+  ])
 
-  return table.map((row) => row.reduce((acc, data, i) => acc + `| ${data}`.padEnd(colWidths[i]), '')).join('\n')
+  return table.map((row) => row.reduce((acc, data, col) => acc + `| ${data}`.padEnd(colWidths[col]), '')).join('\n')
 }
--- test/unit/application/EmissionsTable.test.ts
+++ test/unit/application/EmissionsTable.test.ts
@@ -3,7 +3,7 @@ import EmissionsTable from '@application/EmissionsTable'
 import moment = require('moment')
 
 describe('EmissionsTable', () => {
-  it('do', () => {
+  it('prints out the given estimation results grouped by date', () => {
     const input: EstimationResult[] = [
       {
         timestamp: moment('2020-07-10').toDate(),
@@ -13,6 +13,11 @@ describe('EmissionsTable', () => {
             wattHours: 1,
             co2e: 1,
           },
+          {
+            serviceName: 's3',
+            wattHours: 2,
+            co2e: 2,
+          },
         ],
       },
     ]
@@ -20,9 +25,9 @@ describe('EmissionsTable', () => {
     const result: string = EmissionsTable(input)
 
     expect(result).toEqual(
-      '| Date (UTC)        | EBS Wattage       | EBS CO2e Emissions          \n' +
-        '| 2020-07-10        | 1.00 Watts        | 1.000000 Kg CO2e            \n' +
-        '| Total             | 1.00 Watts        | 1.000000 Kg CO2e            ',
+      '| Date (UTC)        | EBS Wattage       | EBS CO2e Emissions          | S3 Wattage        | S3 CO2e Emissions           \n' +
+        '| 2020-07-10        | 1.00 Watts        | 1.000000 Kg CO2e            | 2.00 Watts        | 2.000000 Kg CO2e            \n' +
+        '| Total             | 1.00 Watts        | 1.000000 Kg CO2e            | 2.00 Watts        | 2.000000 Kg CO2e            ',
     )
   })
 })
