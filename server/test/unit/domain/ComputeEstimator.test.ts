1767f88cbaa91df78825b493013e40a3dbc62730
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2020-07-28 12:09:47 -0500

    fixed linting error in ComputeEstimator.test.ts | Dan

--- server/test/unit/domain/ComputeEstimator.test.ts
+++ server/test/unit/domain/ComputeEstimator.test.ts
@@ -16,7 +16,7 @@ describe('ComputeEstimator', () => {
       {
         co2e: 0.00025690117588675197,
         timestamp: new Date('2020-01-01T00:00:00.000Z'),
-        wattHours: 0.7624799999999999
+        wattHours: 0.7624799999999999,
       },
     ])
   })
