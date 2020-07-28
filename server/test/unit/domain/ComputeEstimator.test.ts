a6b439468ef5bdbfde23bc81c06ae3162c9670c6
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2020-07-28 12:04:24 -0500

    Updated ComputeEstimator to use region-specific AWS watt-hour carbon conversion, Dan

--- server/test/unit/domain/ComputeEstimator.test.ts
+++ server/test/unit/domain/ComputeEstimator.test.ts
@@ -13,7 +13,11 @@ describe('ComputeEstimator', () => {
     const result = new ComputeEstimator().estimate(input, 'us-east-1')
 
     expect(result).toEqual([
-      { co2e: 0.0005391038592, timestamp: new Date('2020-01-01T00:00:00.000Z'), wattHours: 0.7624799999999999 },
+      {
+        co2e: 0.00025690117588675197,
+        timestamp: new Date('2020-01-01T00:00:00.000Z'),
+        wattHours: 0.7624799999999999
+      },
     ])
   })
 })
