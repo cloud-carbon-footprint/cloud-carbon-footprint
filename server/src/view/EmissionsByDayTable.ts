26e0593f59310c6cfbc32f5cf688fdc8e0cee604
kmarkow <kamilmarkow@gmail.com>
2020-07-29 15:54:13 -0400

    Refactor common EmissionTableUtils to a seperate file. Update docs. Kamil | Dan

--- server/src/view/EmissionsByDayTable.ts
+++ server/src/view/EmissionsByDayTable.ts
@@ -1,39 +1,8 @@
 import { EstimationResult } from '@application/EstimationResult'
 import moment from 'moment'
-
-interface Total {
-  wattHours: number
-  co2e: number
-}
-
-type Totals = { [key: string]: Total }
-
-const initialTotals = () => ({
-  ebs: {
-    wattHours: 0,
-    co2e: 0,
-  },
-  s3: {
-    wattHours: 0,
-    co2e: 0,
-  },
-  ec2: {
-    wattHours: 0,
-    co2e: 0,
-  },
-  elasticache: {
-    wattHours: 0,
-    co2e: 0,
-  },
-  sum: {
-    wattHours: 0,
-    co2e: 0,
-  },
-})
+import { displayCo2e, displayServiceName, displayWattHours, initialTotals, Totals } from '@view/EmissionsTableUtils'
 
 const displayDate = (timestamp: Date) => moment(timestamp).utc().format('YYYY-MM-DD')
-const displayWattHours = (wattHours: number) => `${wattHours.toFixed(2)} Watts`
-const displayCo2e = (co2e: number) => `${co2e.toFixed(6)} Kg CO2e`
 const displayService = (totals: Totals, serviceName: string) => [
   displayWattHours(totals[serviceName].wattHours),
   displayCo2e(totals[serviceName].co2e),
@@ -47,11 +16,14 @@ export default function EmissionsByDayTable(
   const colWidths: number[] = [15]
 
   serviceNames.forEach((serviceName) => {
-    headers.push(`${serviceName.toUpperCase()} Wattage`, `${serviceName.toUpperCase()} CO2e Emissions`)
+    headers.push(
+      `${displayServiceName(serviceName)} Watt Hours`,
+      `${displayServiceName(serviceName)} Kg CO2e Emissions`,
+    )
     colWidths.push(20, 25)
   })
 
-  headers.push(`SUM Wattage`, `SUM CO2e Emissions`)
+  headers.push(`SUM Watt Hours`, `SUM Kg CO2e Emissions`)
   colWidths.push(20, 25)
 
   const table: string[][] = [headers]
@@ -65,27 +37,27 @@ export default function EmissionsByDayTable(
 
     estimationResult.estimates.forEach((estimate) => {
       grandTotals[estimate.serviceName].wattHours += estimate.wattHours
-      grandTotals['sum'].wattHours += estimate.wattHours
+      grandTotals['total'].wattHours += estimate.wattHours
       subTotals[estimate.serviceName].wattHours = estimate.wattHours
-      subTotals['sum'].wattHours += estimate.wattHours
+      subTotals['total'].wattHours += estimate.wattHours
 
       grandTotals[estimate.serviceName].co2e += estimate.co2e
-      grandTotals['sum'].co2e += estimate.co2e
+      grandTotals['total'].co2e += estimate.co2e
       subTotals[estimate.serviceName].co2e = estimate.co2e
-      subTotals['sum'].co2e += estimate.co2e
+      subTotals['total'].co2e += estimate.co2e
     })
 
     table.push([
       displayDate(estimationResult.timestamp),
       ...serviceNames.map((serviceName) => displayService(subTotals, serviceName)).flat(),
-      ...displayService(subTotals, 'sum'),
+      ...displayService(subTotals, 'total'),
     ])
   })
 
   table.push([
     'Total',
     ...serviceNames.map((serviceName) => displayService(grandTotals, serviceName)).flat(),
-    ...displayService(grandTotals, 'sum'),
+    ...displayService(grandTotals, 'total'),
   ])
 
   return { table, colWidths }
--- server/src/view/EmissionsByServiceTable.ts
+++ server/src/view/EmissionsByServiceTable.ts
@@ -1,48 +1,5 @@
 import { EstimationResult } from '@application/EstimationResult'
-
-interface Total {
-  wattHours: number
-  co2e: number
-}
-
-type Totals = { [key: string]: Total }
-
-const initialTotals = () => ({
-  ebs: {
-    wattHours: 0,
-    co2e: 0,
-  },
-  s3: {
-    wattHours: 0,
-    co2e: 0,
-  },
-  ec2: {
-    wattHours: 0,
-    co2e: 0,
-  },
-  elasticache: {
-    wattHours: 0,
-    co2e: 0,
-  },
-  total: {
-    wattHours: 0,
-    co2e: 0,
-  },
-})
-
-const displayServiceName = (serviceName: string): string => {
-  const mapping: Record<string, string> = {
-    ebs: 'EBS',
-    s3: 'S3',
-    ec2: 'EC2',
-    elasticache: 'ElastiCache',
-    total: 'Total',
-  }
-  return mapping[serviceName]
-}
-
-const displayWattHours = (wattHours: number) => wattHours.toFixed(2).toString()
-const displayCo2e = (co2e: number) => co2e.toFixed(6).toString()
+import { displayCo2e, displayServiceName, displayWattHours, initialTotals, Totals } from '@view/EmissionsTableUtils'
 
 export default function EmissionsByServiceTable(
   estimations: EstimationResult[],
