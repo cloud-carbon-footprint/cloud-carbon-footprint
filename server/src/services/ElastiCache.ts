2dcf1e2a62b95fa1d6d4799c6e9f1d47f60bf2e3
Zama <jose.zamudio@thoughtworks.com>
2020-07-29 13:31:34 -0500

    Updates elasticache to use cost explorer to get cCPUs | Jose, Adeoulu

--- server/src/services/ElastiCache.ts
+++ server/src/services/ElastiCache.ts
@@ -1,17 +1,26 @@
-import AWS from 'aws-sdk'
+import AWS, { CloudWatch } from 'aws-sdk'
 import ComputeUsage from '@domain/ComputeUsage'
 import ComputeService from '@domain/ComputeService'
-import { CACHE_NODE_TYPES } from '@domain/constants'
+import { AWS_REGIONS, CACHE_NODE_TYPES } from '@domain/constants'
+
+interface RawComputeUsage {
+  cpuUtilization?: number[]
+  vCPUCount?: number
+  timestamp?: Date
+  cpuUtilizationAvg?: number
+}
 
 export default class ElastiCache extends ComputeService {
   serviceName = 'elasticache'
   readonly cloudWatch: AWS.CloudWatch
-  readonly elastiCache: AWS.ElastiCache
+  readonly costExplorer: AWS.CostExplorer
 
   constructor() {
     super()
     this.cloudWatch = new AWS.CloudWatch()
-    this.elastiCache = new AWS.ElastiCache()
+    this.costExplorer = new AWS.CostExplorer({
+      region: AWS_REGIONS.US_EAST_1,
+    })
   }
 
   async getUsage(startDate: Date, endDate: Date): Promise<ComputeUsage[]> {
@@ -33,67 +42,92 @@ export default class ElastiCache extends ComputeService {
     }
 
     const responseMetricData = await this.cloudWatch.getMetricData(params).promise()
-    const responseCacheCluster = await this.elastiCache.describeCacheClusters().promise()
 
-    const vCPUPerCluster = responseCacheCluster.CacheClusters.map(
-      (cluster) => cluster.NumCacheNodes * CACHE_NODE_TYPES[cluster.CacheNodeType],
+    const dataGroupByTimestamp: { [key: string]: RawComputeUsage } = {}
+    const metricDataResults = responseMetricData.MetricDataResults
+
+    this.aggregateCPUUtilizationByDay(metricDataResults, dataGroupByTimestamp)
+    this.calculateAverages(dataGroupByTimestamp)
+
+    const vcpusByDate = await this.getTotalVCpusByDate(
+      startDate.toISOString().substr(0, 10),
+      endDate.toISOString().substr(0, 10),
     )
 
-    const totalNumberOfvCpus = vCPUPerCluster.reduce((acc, x) => acc + x, 0)
+    // Build result and add vCpus
+    const estimationsByDay: { [timestamp: string]: ComputeUsage } = {}
+    Object.values(dataGroupByTimestamp).forEach((a) => {
+      const timestamp = new Date(a.timestamp)
+      const date = timestamp.toISOString().substr(0, 10)
 
-    interface RawComputeUsage {
-      cpuUtilization?: number[]
-      vCPUCount?: number
-      timestamp?: Date
-      cpuUtilizationAvg?: number
-    }
-    const result: { [key: string]: RawComputeUsage } = {}
-
-    // Aggregate CPU Utilization
-    const metricDataResults = responseMetricData.MetricDataResults
-    const cpuUtilizationData = metricDataResults.filter((a) => a.Id === 'cpuUtilization')
-    cpuUtilizationData.forEach((instanceCPUUtilization) => {
-      instanceCPUUtilization.Timestamps.forEach((timestamp, i) => {
-        const key = new Date(timestamp).toISOString()
-        if (!result[key]) result[key] = { cpuUtilization: [instanceCPUUtilization.Values[i]], timestamp: timestamp }
-        result[key].cpuUtilization.push()
-      })
+      estimationsByDay[date] = {
+        timestamp: new Date(date),
+        cpuUtilizationAverage: a.cpuUtilizationAvg,
+        numberOfvCpus: vcpusByDate[date],
+      }
     })
 
-    // Add vCPU to result object
-    // const vCPUData = metricDataResults.filter((a) => a.Id === 'vCPUs')
-    // vCPUData[0].Timestamps.forEach((timestamp, i) => {
-    //   const key = new Date(timestamp).toISOString()
-    //   if (!result[key]) result[key] = { cpuUtilization: [], timestamp: timestamp }
-    //   result[key].vCPUCount = vCPUData[0].Values[i]
-    // })
+    return Object.values(estimationsByDay)
+  }
 
-    // Apply estimation formula
-    Object.values(result).forEach((a) => {
+  private calculateAverages(dataGroupByTimestamp: { [p: string]: RawComputeUsage }) {
+    Object.values(dataGroupByTimestamp).forEach((a) => {
       a.cpuUtilizationAvg =
         a.cpuUtilization.reduce((acc, a) => {
           return acc + a
         }, 0) / a.cpuUtilization.length
     })
+  }
 
-    // Aggregate by date
-    const estimationsByDay: { [timestamp: string]: ComputeUsage } = {}
-    Object.values(result).forEach((a) => {
-      const timestamp = new Date(a.timestamp)
-      const date = timestamp.toISOString().substr(0, 10)
-
-      if (!estimationsByDay[date]) {
-        estimationsByDay[date] = {
-          timestamp: new Date(date),
-          cpuUtilizationAverage: a.cpuUtilizationAvg,
-          numberOfvCpus: totalNumberOfvCpus,
-        }
-      } else {
-        estimationsByDay[date].cpuUtilizationAverage += a.cpuUtilizationAvg
-        // estimationsByDay[date].numberOfvCpus += a.vCPUCount
-      }
+  private aggregateCPUUtilizationByDay(
+    metricDataResults: CloudWatch.MetricDataResult[],
+    dataGroupByTimestamp: { [p: string]: RawComputeUsage },
+  ) {
+    const cpuUtilizationData = metricDataResults.filter((a) => a.Id === 'cpuUtilization')
+    cpuUtilizationData.forEach((allClustersCPUUtilization) => {
+      allClustersCPUUtilization.Timestamps.forEach((timestamp, i) => {
+        const key = new Date(timestamp).toISOString().substr(0, 10)
+        if (!dataGroupByTimestamp[key])
+          dataGroupByTimestamp[key] = {
+            cpuUtilization: [allClustersCPUUtilization.Values[i]],
+            timestamp: timestamp,
+          }
+        else dataGroupByTimestamp[key].cpuUtilization.push(allClustersCPUUtilization.Values[i])
+      })
     })
+  }
 
-    return Object.values(estimationsByDay)
+  private async getTotalVCpusByDate(startDate: string, endDate: string): Promise<{ [timestamp: string]: number }> {
+    const params = {
+      TimePeriod: {
+        Start: startDate,
+        End: endDate,
+      },
+      Filter: {
+        Dimensions: {
+          Key: 'USAGE_TYPE_GROUP',
+          Values: ['ElastiCache: Running Hours'],
+        },
+      },
+      Granularity: 'DAILY',
+      GroupBy: [
+        {
+          Key: 'USAGE_TYPE',
+          Type: 'DIMENSION',
+        },
+      ],
+      Metrics: ['UsageQuantity'],
+    }
+
+    const vcpusByDate: { [timestamp: string]: number } = {}
+    const response = await this.costExplorer.getCostAndUsage(params).promise()
+    response.ResultsByTime.reduce((acc, result) => {
+      acc[result.TimePeriod.Start] = result.Groups.reduce((sum, group) => {
+        return sum + Number.parseInt(group.Metrics.UsageQuantity.Amount) * CACHE_NODE_TYPES[group.Keys[0].split(':')[1]]
+      }, 0)
+      return acc
+    }, vcpusByDate)
+
+    return vcpusByDate
   }
 }
--- server/test/unit/services/ElastiCache.test.ts
+++ server/test/unit/services/ElastiCache.test.ts
@@ -2,57 +2,380 @@ import AWSMock from 'aws-sdk-mock'
 import AWS from 'aws-sdk'
 
 import ElastiCache from '@services/ElastiCache'
-import { elastiCacheMockResponse, elastiCacheMockDescribeCacheClusters } from '@fixtures'
+import { elastiCacheMockResponse } from '@fixtures'
 
 beforeAll(() => {
   AWSMock.setSDKInstance(AWS)
 })
+
+function cloudwatchRequest(startTimestamp: string, endTimestamp: string) {
+  return {
+    StartTime: new Date(startTimestamp),
+    EndTime: new Date(endTimestamp),
+    MetricDataQueries: [
+      {
+        Id: 'cpuUtilizationWithEmptyValues',
+        Expression: "SEARCH('{AWS/ElastiCache} MetricName=\"CPUUtilization\"', 'Average', 3600)",
+        ReturnData: false,
+      },
+      {
+        Id: 'cpuUtilization',
+        Expression: 'REMOVE_EMPTY(cpuUtilizationWithEmptyValues)',
+      },
+    ],
+    ScanBy: 'TimestampAscending',
+  }
+}
+
+function costExplorerRequest(startDate: string, endDate: string) {
+  return {
+    TimePeriod: {
+      Start: startDate,
+      End: endDate,
+    },
+    Filter: {
+      Dimensions: {
+        Key: 'USAGE_TYPE_GROUP',
+        Values: ['ElastiCache: Running Hours'],
+      },
+    },
+    Granularity: 'DAILY',
+    GroupBy: [
+      {
+        Key: 'USAGE_TYPE',
+        Type: 'DIMENSION',
+      },
+    ],
+    Metrics: ['UsageQuantity'],
+  }
+}
 
 describe('ElastiCache', () => {
   afterEach(() => {
     AWSMock.restore()
   })
 
-  it('gets Elasticache usage', async () => {
+  it('should return the usage of two hours of different days', async () => {
     AWSMock.mock(
       'CloudWatch',
       'getMetricData',
       (params: AWS.CloudWatch.GetMetricDataInput, callback: (a: Error, response: any) => any) => {
-        expect(params).toEqual({
-          StartTime: new Date('2020-07-10T00:00:00.000Z'),
-          EndTime: new Date('2020-07-21T00:00:00.000Z'),
-          MetricDataQueries: [
-            {
-              Id: 'cpuUtilizationWithEmptyValues',
-              Expression: "SEARCH('{AWS/ElastiCache} MetricName=\"CPUUtilization\"', 'Average', 3600)",
-              ReturnData: false,
-            },
-            {
-              Id: 'cpuUtilization',
-              Expression: 'REMOVE_EMPTY(cpuUtilizationWithEmptyValues)',
-            },
-          ],
-          ScanBy: 'TimestampAscending',
-        })
-
+        expect(params).toEqual(cloudwatchRequest('2020-07-19T00:00:00.000Z', '2020-07-21T00:00:00.000Z'))
         callback(null, elastiCacheMockResponse)
       },
     )
 
-    AWSMock.mock('ElastiCache', 'describeCacheClusters', (callback: (a: Error, response: any) => any) => {
-      callback(null, elastiCacheMockDescribeCacheClusters)
-    })
+    AWSMock.mock(
+      'CostExplorer',
+      'getCostAndUsage',
+      (params: AWS.CostExplorer.GetCostAndUsageRequest, callback: (a: Error, response: any) => any) => {
+        expect(params).toEqual(costExplorerRequest('2020-07-19', '2020-07-21'))
+
+        callback(null, {
+          ResultsByTime: [
+            {
+              TimePeriod: {
+                Start: '2020-07-19',
+                End: '2020-07-20',
+              },
+              Groups: [
+                {
+                  Keys: ['NodeUsage:cache.t3.medium'],
+                  Metrics: {
+                    UsageQuantity: {
+                      Amount: '2',
+                    },
+                  },
+                },
+              ],
+            },
+            {
+              TimePeriod: {
+                Start: '2020-07-20',
+                End: '2020-07-21',
+              },
+              Groups: [
+                {
+                  Keys: ['NodeUsage:cache.t3.medium'],
+                  Metrics: {
+                    UsageQuantity: {
+                      Amount: '2',
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+        })
+      },
+    )
 
     const elasticacheService = new ElastiCache()
 
-    const result = await elasticacheService.getUsage(
-      new Date('2020-07-10T00:00:00.000Z'),
+    const usageByHour = await elasticacheService.getUsage(
+      new Date('2020-07-19T00:00:00.000Z'),
       new Date('2020-07-21T00:00:00.000Z'),
     )
 
-    expect(result).toEqual([
+    expect(usageByHour).toEqual([
       { cpuUtilizationAverage: 1.0456, numberOfvCpus: 4, timestamp: new Date('2020-07-19T00:00:00.000Z') },
       { cpuUtilizationAverage: 2.03242, numberOfvCpus: 4, timestamp: new Date('2020-07-20T00:00:00.000Z') },
     ])
   })
+
+  it('should return empty list when no usage', async () => {
+    AWSMock.mock(
+      'CloudWatch',
+      'getMetricData',
+      (params: AWS.CloudWatch.GetMetricDataInput, callback: (a: Error, response: any) => any) => {
+        expect(params).toEqual(cloudwatchRequest('2020-07-10T00:00:00.000Z', '2020-07-11T00:00:00.000Z'))
+
+        callback(null, {
+          MetricDataResults: [
+            {
+              Id: 'cpuUtilization',
+              Label: 'cpuUtilization',
+              Timestamps: [],
+              Values: [],
+            },
+          ],
+        })
+      },
+    )
+
+    AWSMock.mock(
+      'CostExplorer',
+      'getCostAndUsage',
+      (params: AWS.CostExplorer.GetCostAndUsageRequest, callback: (a: Error, response: any) => any) => {
+        expect(params).toEqual(costExplorerRequest('2020-07-10', '2020-07-11'))
+        callback(null, {
+          ResultsByTime: [
+            {
+              TimePeriod: {
+                Start: '2020-07-10',
+                End: '2020-07-11',
+              },
+              Groups: [],
+            },
+          ],
+        })
+      },
+    )
+
+    const elasticacheService = new ElastiCache()
+
+    const usageByHour = await elasticacheService.getUsage(
+      new Date('2020-07-10T00:00:00.000Z'),
+      new Date('2020-07-11T00:00:00.000Z'),
+    )
+
+    expect(usageByHour).toEqual([])
+  })
+
+  it('should return the usage when two different cache instances types were used', async () => {
+    AWSMock.mock(
+      'CloudWatch',
+      'getMetricData',
+      (params: AWS.CloudWatch.GetMetricDataInput, callback: (a: Error, response: any) => any) => {
+        expect(params).toEqual(cloudwatchRequest('2020-07-10T00:00:00.000Z', '2020-07-11T00:00:00.000Z'))
+
+        callback(null, {
+          MetricDataResults: [
+            {
+              Id: 'cpuUtilization',
+              Label: 'cpuUtilization',
+              Timestamps: [new Date('2020-07-10T22:00:00.000Z')],
+              Values: [50],
+            },
+          ],
+        })
+      },
+    )
+
+    AWSMock.mock(
+      'CostExplorer',
+      'getCostAndUsage',
+      (params: AWS.CostExplorer.GetCostAndUsageRequest, callback: (a: Error, response: any) => any) => {
+        expect(params).toEqual(costExplorerRequest('2020-07-10', '2020-07-11'))
+        callback(null, {
+          ResultsByTime: [
+            {
+              TimePeriod: {
+                Start: '2020-07-10',
+                End: '2020-07-11',
+              },
+              Groups: [
+                {
+                  Keys: ['USE2-NodeUsage:cache.t3.medium'],
+                  Metrics: {
+                    UsageQuantity: {
+                      Amount: '3',
+                    },
+                  },
+                },
+                {
+                  Keys: ['USE2-NodeUsage:cache.t2.micro'],
+                  Metrics: {
+                    UsageQuantity: {
+                      Amount: '2',
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+        })
+      },
+    )
+
+    const elasticacheService = new ElastiCache()
+
+    const usageByHour = await elasticacheService.getUsage(
+      new Date('2020-07-10T00:00:00.000Z'),
+      new Date('2020-07-11T00:00:00.000Z'),
+    )
+
+    expect(usageByHour).toEqual([
+      { cpuUtilizationAverage: 50, numberOfvCpus: 8, timestamp: new Date('2020-07-10T00:00:00.000Z') },
+    ])
+  })
+
+  it('should return the usage when two different cache instances types in different hours were used', async () => {
+    AWSMock.mock(
+      'CloudWatch',
+      'getMetricData',
+      (params: AWS.CloudWatch.GetMetricDataInput, callback: (a: Error, response: any) => any) => {
+        expect(params).toEqual(cloudwatchRequest('2020-07-10T00:00:00.000Z', '2020-07-11T00:00:00.000Z'))
+
+        callback(null, {
+          MetricDataResults: [
+            {
+              Id: 'cpuUtilization',
+              Label: 'cpuUtilization',
+              Timestamps: [new Date('2020-07-10T22:00:00.000Z'), new Date('2020-07-10T22:06:00.000Z')],
+              Values: [50, 70],
+            },
+          ],
+        })
+      },
+    )
+
+    AWSMock.mock(
+      'CostExplorer',
+      'getCostAndUsage',
+      (params: AWS.CostExplorer.GetCostAndUsageRequest, callback: (a: Error, response: any) => any) => {
+        expect(params).toEqual(costExplorerRequest('2020-07-10', '2020-07-11'))
+        callback(null, {
+          ResultsByTime: [
+            {
+              TimePeriod: {
+                Start: '2020-07-10',
+                End: '2020-07-11',
+              },
+              Groups: [
+                {
+                  Keys: ['USE2-NodeUsage:cache.t3.medium'],
+                  Metrics: {
+                    UsageQuantity: {
+                      Amount: '2',
+                    },
+                  },
+                },
+                {
+                  Keys: ['USE2-NodeUsage:cache.t2.micro'],
+                  Metrics: {
+                    UsageQuantity: {
+                      Amount: '2',
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+        })
+      },
+    )
+
+    const elasticacheService = new ElastiCache()
+
+    const usageByHour = await elasticacheService.getUsage(
+      new Date('2020-07-10T00:00:00.000Z'),
+      new Date('2020-07-11T00:00:00.000Z'),
+    )
+
+    expect(usageByHour).toEqual([
+      { cpuUtilizationAverage: 60, numberOfvCpus: 6, timestamp: new Date('2020-07-10T00:00:00.000Z') },
+    ])
+  })
+
+  it('should return the usage when two different cache instances types in different hours were used', async () => {
+    AWSMock.mock(
+      'CloudWatch',
+      'getMetricData',
+      (params: AWS.CloudWatch.GetMetricDataInput, callback: (a: Error, response: any) => any) => {
+        expect(params).toEqual(cloudwatchRequest('2020-07-10T00:00:00.000Z', '2020-07-11T00:00:00.000Z'))
+
+        callback(null, {
+          MetricDataResults: [
+            {
+              Id: 'cpuUtilization',
+              Label: 'cpuUtilization',
+              Timestamps: [new Date('2020-07-10T22:00:00.000Z'), new Date('2020-07-10T22:06:00.000Z')],
+              Values: [50, 70],
+            },
+          ],
+        })
+      },
+    )
+
+    AWSMock.mock(
+      'CostExplorer',
+      'getCostAndUsage',
+      (params: AWS.CostExplorer.GetCostAndUsageRequest, callback: (a: Error, response: any) => any) => {
+        expect(params).toEqual(costExplorerRequest('2020-07-10', '2020-07-11'))
+        callback(null, {
+          ResultsByTime: [
+            {
+              TimePeriod: {
+                Start: '2020-07-10',
+                End: '2020-07-11',
+              },
+              Groups: [
+                {
+                  Keys: ['USE2-NodeUsage:cache.t3.medium'],
+                  Metrics: {
+                    UsageQuantity: {
+                      Amount: '2',
+                    },
+                  },
+                },
+                {
+                  Keys: ['USE2-NodeUsage:cache.t2.micro'],
+                  Metrics: {
+                    UsageQuantity: {
+                      Amount: '2',
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+        })
+      },
+    )
+
+    const elasticacheService = new ElastiCache()
+
+    const usageByHour = await elasticacheService.getUsage(
+      new Date('2020-07-10T00:00:00.000Z'),
+      new Date('2020-07-11T00:00:00.000Z'),
+    )
+
+    expect(usageByHour).toEqual([
+      { cpuUtilizationAverage: 60, numberOfvCpus: 6, timestamp: new Date('2020-07-10T00:00:00.000Z') },
+    ])
+  })
 })
+
+// TODO: parse float Amount?
+// TODO: add all cache type instances to constants
