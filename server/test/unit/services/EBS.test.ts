328499e8c3202f138c8a4b5e4e0996ecaf80d61a
Zama <jose.zamudio@thoughtworks.com>
2020-08-03 18:50:45 -0500

    WIP, update EBS tests | Jose

--- server/test/unit/services/EBS.test.ts
+++ server/test/unit/services/EBS.test.ts
@@ -33,24 +33,7 @@ describe('Ebs', () => {
           },
         })
 
-        callback(null, {
-          ResultsByTime: [
-            {
-              Estimated: false,
-              Groups: [],
-              TimePeriod: {
-                End: '2020-06-28',
-                Start: '2020-06-27',
-              },
-              Total: {
-                UsageQuantity: {
-                  Amount: '1.2120679',
-                  Unit: 'GB-Month',
-                },
-              },
-            },
-          ],
-        })
+        callback(null, buildAwsCostExplorerGetCostAndUsageResponse([{ start: '2020-06-27', value: '1.2120679' }]))
       },
     )
 
@@ -72,38 +55,13 @@ describe('Ebs', () => {
       'CostExplorer',
       'getCostAndUsage',
       (params: AWS.CostExplorer.GetCostAndUsageRequest, callback: (a: Error, response: any) => any) => {
-        callback(null, {
-          ResultsByTime: [
-            {
-              Estimated: false,
-              Groups: [],
-              TimePeriod: {
-                End: '2020-06-28',
-                Start: '2020-06-27',
-              },
-              Total: {
-                UsageQuantity: {
-                  Amount: '0',
-                  Unit: 'GB-Month',
-                },
-              },
-            },
-            {
-              Estimated: false,
-              Groups: [],
-              TimePeriod: {
-                End: '2020-06-28',
-                Start: '2020-06-27',
-              },
-              Total: {
-                UsageQuantity: {
-                  Amount: '1.2120679',
-                  Unit: 'GB-Month',
-                },
-              },
-            },
-          ],
-        })
+        callback(
+          null,
+          buildAwsCostExplorerGetCostAndUsageResponse([
+            { start: '2020-06-27', value: '0' },
+            { start: '2020-06-27', value: '1.2120679' },
+          ]),
+        )
       },
     )
 
@@ -117,22 +75,6 @@ describe('Ebs', () => {
         timestamp: new Date('2020-06-27T00:00:00Z'),
       },
     ])
-  })
-
-  it('returns empty list when no ResultsByTime', async () => {
-    AWSMock.mock(
-      'CostExplorer',
-      'getCostAndUsage',
-      (params: AWS.CostExplorer.GetCostAndUsageRequest, callback: (a: Error, response: any) => any) => {
-        callback(null, {})
-      },
-    )
-
-    const ebsService = new EBS()
-
-    const result = await ebsService.getUsage(new Date('2020-01-27T00:00:00Z'), new Date('2020-01-30T00:00:00Z'))
-
-    expect(result).toEqual([])
   })
 
   it('returns empty list when empty ResultsByTime', async () => {
@@ -153,7 +95,7 @@ describe('Ebs', () => {
     expect(result).toEqual([])
   })
 
-  it('filters out results with no TimePeriod', async () => {
+  it('should return empty array if no usage', async () => {
     AWSMock.mock(
       'CostExplorer',
       'getCostAndUsage',
@@ -161,28 +103,10 @@ describe('Ebs', () => {
         callback(null, {
           ResultsByTime: [
             {
-              Estimated: false,
-              Groups: [],
-              Total: {
-                UsageQuantity: {
-                  Amount: '0',
-                  Unit: 'GB-Month',
-                },
-              },
-            },
-            {
-              Estimated: false,
-              Groups: [],
               TimePeriod: {
-                End: '2020-06-28',
-                Start: '2020-06-27',
+                Start: '2020-01-27',
               },
-              Total: {
-                UsageQuantity: {
-                  Amount: '1.2120679',
-                  Unit: 'GB-Month',
-                },
-              },
+              Groups: [],
             },
           ],
         })
@@ -193,156 +117,7 @@ describe('Ebs', () => {
 
     const result = await ebsService.getUsage(new Date('2020-01-27T00:00:00Z'), new Date('2020-01-30T00:00:00Z'))
 
-    expect(result).toEqual([
-      {
-        sizeGb: 36.362037,
-        timestamp: new Date('2020-06-27T00:00:00Z'),
-      },
-    ])
-  })
-
-  it('filters out results with no StartDate', async () => {
-    AWSMock.mock(
-      'CostExplorer',
-      'getCostAndUsage',
-      (params: AWS.CostExplorer.GetCostAndUsageRequest, callback: (a: Error, response: any) => any) => {
-        callback(null, {
-          ResultsByTime: [
-            {
-              Estimated: false,
-              Groups: [],
-              TimePeriod: {
-                End: '2020-06-28',
-              },
-              Total: {
-                UsageQuantity: {
-                  Amount: '0',
-                  Unit: 'GB-Month',
-                },
-              },
-            },
-            {
-              Estimated: false,
-              Groups: [],
-              TimePeriod: {
-                End: '2020-06-28',
-                Start: '2020-06-27',
-              },
-              Total: {
-                UsageQuantity: {
-                  Amount: '1.2120679',
-                  Unit: 'GB-Month',
-                },
-              },
-            },
-          ],
-        })
-      },
-    )
-
-    const ebsService = new EBS()
-
-    const result = await ebsService.getUsage(new Date('2020-01-27T00:00:00Z'), new Date('2020-01-30T00:00:00Z'))
-
-    expect(result).toEqual([
-      {
-        sizeGb: 36.362037,
-        timestamp: new Date('2020-06-27T00:00:00Z'),
-      },
-    ])
-  })
-
-  it('filters out results with no Total', async () => {
-    AWSMock.mock(
-      'CostExplorer',
-      'getCostAndUsage',
-      (params: AWS.CostExplorer.GetCostAndUsageRequest, callback: (a: Error, response: any) => any) => {
-        callback(null, {
-          ResultsByTime: [
-            {
-              Estimated: false,
-              Groups: [],
-              TimePeriod: {
-                End: '2020-06-28',
-                Start: '2020-06-27',
-              },
-            },
-            {
-              Estimated: false,
-              Groups: [],
-              TimePeriod: {
-                End: '2020-06-28',
-                Start: '2020-06-27',
-              },
-              Total: {
-                UsageQuantity: {
-                  Amount: '1.2120679',
-                  Unit: 'GB-Month',
-                },
-              },
-            },
-          ],
-        })
-      },
-    )
-
-    const ebsService = new EBS()
-
-    const result = await ebsService.getUsage(new Date('2020-01-27T00:00:00Z'), new Date('2020-01-30T00:00:00Z'))
-
-    expect(result).toEqual([
-      {
-        sizeGb: 36.362037,
-        timestamp: new Date('2020-06-27T00:00:00Z'),
-      },
-    ])
-  })
-
-  it('filters out results with no UsageQuantity', async () => {
-    AWSMock.mock(
-      'CostExplorer',
-      'getCostAndUsage',
-      (params: AWS.CostExplorer.GetCostAndUsageRequest, callback: (a: Error, response: any) => any) => {
-        callback(null, {
-          ResultsByTime: [
-            {
-              Estimated: false,
-              Groups: [],
-              TimePeriod: {
-                End: '2020-06-28',
-                Start: '2020-06-27',
-              },
-              Total: {},
-            },
-            {
-              Estimated: false,
-              Groups: [],
-              TimePeriod: {
-                End: '2020-06-28',
-                Start: '2020-06-27',
-              },
-              Total: {
-                UsageQuantity: {
-                  Amount: '1.2120679',
-                  Unit: 'GB-Month',
-                },
-              },
-            },
-          ],
-        })
-      },
-    )
-
-    const ebsService = new EBS()
-
-    const result = await ebsService.getUsage(new Date('2020-01-27T00:00:00Z'), new Date('2020-01-30T00:00:00Z'))
-
-    expect(result).toEqual([
-      {
-        sizeGb: 36.362037,
-        timestamp: new Date('2020-06-27T00:00:00Z'),
-      },
-    ])
+    expect(result).toEqual([])
   })
 
   it('filters out results with no Amount', async () => {
