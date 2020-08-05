0023a259d6d2ba56cb816706ad3c2a7b57048792
Zama <jose.zamudio@thoughtworks.com>
2020-08-05 12:51:21 -0500

    More refactor EBS and RDS Storage | Jose

--- server/src/services/RDSStorage.ts
+++ server/src/services/RDSStorage.ts
@@ -1,44 +1,20 @@
 import AWS from 'aws-sdk'
-import { AWS_POWER_USAGE_EFFECTIVENESS, AWS_REGIONS, HDDCOEFFICIENT, SSDCOEFFICIENT } from '@domain/constants'
 import { GetCostAndUsageRequest } from 'aws-sdk/clients/costexplorer'
 import ICloudService from '@domain/ICloudService'
-import { StorageEstimator } from '@domain/StorageEstimator'
-import { DiskType, getUsageFromCostExplorer, MutableFootprintEstimate, VolumeUsage } from '@services/StorageUsageMapper'
+import {
+  DiskType,
+  getEstimatesFromCostExplorer,
+  getUsageFromCostExplorer,
+  VolumeUsage,
+} from '@services/StorageUsageMapper'
+import FootprintEstimate from '@domain/FootprintEstimate'
 
 export default class RDSStorage implements ICloudService {
   serviceName = 'rds-storage'
-  readonly costExplorer: AWS.CostExplorer
-  readonly ssdEstimator: StorageEstimator
-  readonly hddEstimator: StorageEstimator
 
-  constructor() {
-    this.ssdEstimator = new StorageEstimator(SSDCOEFFICIENT, AWS_POWER_USAGE_EFFECTIVENESS)
-    this.hddEstimator = new StorageEstimator(HDDCOEFFICIENT, AWS_POWER_USAGE_EFFECTIVENESS)
-    this.costExplorer = new AWS.CostExplorer({
-      region: AWS_REGIONS.US_EAST_1, //must be us-east-1 to work
-    })
-  }
-
-  async getEstimates(start: Date, end: Date, region: string): Promise<MutableFootprintEstimate[]> {
-    const usage = await this.getUsage(start, end)
-    const ssdUsage = usage.filter(({ diskType: diskType }) => DiskType.SSD === diskType)
-    const hddUsage = usage.filter(({ diskType: diskType }) => DiskType.HDD === diskType)
-    const footprintEstimates = [
-      ...this.ssdEstimator.estimate(ssdUsage, region),
-      ...this.hddEstimator.estimate(hddUsage, region),
-    ]
-
-    return Object.values(
-      footprintEstimates.reduce((acc: { [key: string]: MutableFootprintEstimate }, estimate) => {
-        if (!acc[estimate.timestamp.toISOString()]) {
-          acc[estimate.timestamp.toISOString()] = estimate
-          return acc
-        }
-        acc[estimate.timestamp.toISOString()].co2e += estimate.co2e
-        acc[estimate.timestamp.toISOString()].wattHours += estimate.wattHours
-        return acc
-      }, {}),
-    )
+  async getEstimates(start: Date, end: Date, region: string): Promise<FootprintEstimate[]> {
+    const usage: VolumeUsage[] = await this.getUsage(start, end)
+    return getEstimatesFromCostExplorer(start, end, region, usage)
   }
 
   async getUsage(startDate: Date, endDate: Date): Promise<VolumeUsage[]> {
