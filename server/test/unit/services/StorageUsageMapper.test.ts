0208af05c57eb864de9c7a9e4f390d29de6b8af7
megangoodling <megan.goodling@thoughtworks.com>
2020-08-06 12:48:27 -0400

    add pagination for cost explorer for storage usage mapper | Kamil, Megan

---
+++ server/test/unit/services/StorageUsageMapper.test.ts
@@ -0,0 +1,96 @@
+import AWSMock from 'aws-sdk-mock'
+import AWS from 'aws-sdk'
+import { getUsageFromCostExplorer, DiskType, VolumeUsage } from '@services/StorageUsageMapper'
+
+beforeAll(() => {
+  AWS.config.update({ region: 'us-east-1' })
+  AWSMock.setSDKInstance(AWS)
+})
+
+describe('Ebs', () => {
+  const mockFunction = jest.fn()
+  mockFunction
+    .mockReturnValueOnce(
+      buildAwsCostExplorerGetCostAndUsageResponse(
+        [{ start: '2020-06-27', value: '1.2120679', types: ['EBS:VolumeUsage.gp2'] }],
+        'tokenToNextPage',
+      ),
+    )
+    .mockReturnValueOnce(
+      buildAwsCostExplorerGetCostAndUsageResponse(
+        [{ start: '2020-06-28', value: '1.2120679', types: ['EBS:VolumeUsage.gp2'] }],
+        null,
+      ),
+    )
+
+  afterEach(() => {
+    AWSMock.restore()
+    jest.restoreAllMocks()
+  })
+
+  it('test', async () => {
+    const params = {
+      TimePeriod: {
+        Start: '2019-08-06',
+        End: '2020-08-06',
+      },
+      Granularity: 'DAILY',
+      Metrics: [
+        'AmortizedCost',
+        'BlendedCost',
+        'NetAmortizedCost',
+        'NetUnblendedCost',
+        'NormalizedUsageAmount',
+        'UnblendedCost',
+        'UsageQuantity',
+      ],
+      GroupBy: [
+        {
+          Key: 'USAGE_TYPE',
+          Type: 'DIMENSION',
+        },
+      ],
+    }
+    AWSMock.mock(
+      'CostExplorer',
+      'getCostAndUsage',
+      (request: AWS.CostExplorer.GetCostAndUsageRequest, callback: (a: Error, response: any) => any) => {
+        callback(null, mockFunction())
+      },
+    )
+    const res: VolumeUsage[] = await getUsageFromCostExplorer(params, () => DiskType.SSD)
+
+    expect(res).toEqual([
+      { diskType: 'SSD', sizeGb: 36.362037, timestamp: new Date('2020-06-27T00:00:00.000Z') },
+      { diskType: 'SSD', sizeGb: 36.362037, timestamp: new Date('2020-06-28T00:00:00.000Z') },
+    ])
+  })
+})
+
+function buildAwsCostExplorerGetCostAndUsageResponse(
+  data: { start: string; value: string; types: string[] }[],
+  nextPageToken: string,
+) {
+  return {
+    NextPageToken: nextPageToken,
+    GroupDefinitions: [
+      {
+        Type: 'DIMENSION',
+        Key: 'USAGE_TYPE',
+      },
+    ],
+    ResultsByTime: data.map(({ start, value, types }) => {
+      return {
+        TimePeriod: {
+          Start: start,
+        },
+        Groups: [
+          {
+            Keys: types,
+            Metrics: { UsageQuantity: { Amount: value, Unit: 'GB-Month' } },
+          },
+        ],
+      }
+    }),
+  }
+}
