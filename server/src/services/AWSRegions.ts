2bf76209ec052b803f2c46ce21c2eff815dc60d8
megangoodling <megan.goodling@thoughtworks.com>
2020-08-13 10:30:56 -0400

    add documentation to read me about how to specify regions | Megan

--- server/src/services/AWSRegions.ts
+++ server/src/services/AWSRegions.ts
@@ -1,6 +1,6 @@
 export enum AWS_REGIONS {
-  US_EAST_2 = 'us-east-2',
   US_EAST_1 = 'us-east-1',
+  US_EAST_2 = 'us-east-2',
   US_WEST_1 = 'us-west-1',
   US_WEST_2 = 'us-west-2',
   AF_SOUTH_1 = 'af-south-1',
