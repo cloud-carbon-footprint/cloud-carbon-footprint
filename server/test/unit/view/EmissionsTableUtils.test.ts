409ba4ba61e17d0330354eeec419cf670a3ec08d
megangoodling <megan.goodling@thoughtworks.com>
2020-08-14 11:58:07 -0400

    added config file and made service list more dynamic in EmissionsTableUtils | Vini, Megan

---
+++ server/test/unit/view/EmissionsTableUtils.test.ts
@@ -0,0 +1,34 @@
+import { initialTotals } from '@view/EmissionsTableUtils'
+
+describe('emissions table utils', () => {
+  it('generate initial totals from list of service objects', () => {
+    const expectedInitials = {
+      ebs: {
+        wattHours: 0,
+        co2e: 0,
+      },
+      s3: {
+        wattHours: 0,
+        co2e: 0,
+      },
+      ec2: {
+        wattHours: 0,
+        co2e: 0,
+      },
+      elasticache: {
+        wattHours: 0,
+        co2e: 0,
+      },
+      rds: {
+        wattHours: 0,
+        co2e: 0,
+      },
+      total: {
+        wattHours: 0,
+        co2e: 0,
+      },
+    }
+
+    expect(initialTotals()).toEqual(expectedInitials)
+  })
+})
