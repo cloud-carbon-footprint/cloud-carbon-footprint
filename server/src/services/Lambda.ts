85e40e4b1451790eb84095de1a5d8080725f4db3
vivian <vivian_liao@gap.com>
2020-08-14 10:23:02 -0700

    lint fix | Jose, Vivian

--- server/src/services/Lambda.ts
+++ server/src/services/Lambda.ts
@@ -1,75 +1,75 @@
-import ICloudService from "@domain/ICloudService";
+import ICloudService from '@domain/ICloudService'
 import FootprintEstimate from '@domain/FootprintEstimate'
-import {estimateCo2} from '@domain/FootprintEstimationConstants'
-import AWS from "aws-sdk"
+import { estimateCo2 } from '@domain/FootprintEstimationConstants'
+import AWS from 'aws-sdk'
 
 export default class Lambda implements ICloudService {
-    serviceName: "lambda";
+  serviceName: 'lambda'
 
-    async getEstimates(start: Date, end: Date, region: string): Promise<FootprintEstimate[]> {
-        const cloudWatchLogs = new AWS.CloudWatchLogs();
-        const groupNames = await this.getLambdaLogGroupNames(cloudWatchLogs)
-        const queryId = await this.runQuery(cloudWatchLogs, start, end, groupNames)
-        const usage = await this.getResults(cloudWatchLogs, queryId)
+  async getEstimates(start: Date, end: Date, region: string): Promise<FootprintEstimate[]> {
+    const cloudWatchLogs = new AWS.CloudWatchLogs()
+    const groupNames = await this.getLambdaLogGroupNames(cloudWatchLogs)
+    const queryId = await this.runQuery(cloudWatchLogs, start, end, groupNames)
+    const usage = await this.getResults(cloudWatchLogs, queryId)
 
-        return usage.results.map(resultByDate=>{
-            const timestampField = resultByDate[0]
-            const wattsField = resultByDate[1]
+    return usage.results.map((resultByDate) => {
+      const timestampField = resultByDate[0]
+      const wattsField = resultByDate[1]
 
-            const timestamp = new Date(timestampField.value.substr(0,10))
-            const wattHours = Number.parseFloat(wattsField.value)
-            const co2e =  estimateCo2(wattHours, region)
-            return {
-                timestamp,
-                wattHours,
-                co2e
-            }
-        })
+      const timestamp = new Date(timestampField.value.substr(0, 10))
+      const wattHours = Number.parseFloat(wattsField.value)
+      const co2e = estimateCo2(wattHours, region)
+      return {
+        timestamp,
+        wattHours,
+        co2e,
+      }
+    })
+  }
+
+  private async getLambdaLogGroupNames(cw: AWS.CloudWatchLogs): Promise<string[]> {
+    const params = {
+      logGroupNamePrefix: '/aws/lambda',
     }
 
-    private async getLambdaLogGroupNames(cw: AWS.CloudWatchLogs): Promise<string[]> {
-        let params = {
-            logGroupNamePrefix: '/aws/lambda',
-        };
+    const data = await cw.describeLogGroups(params).promise()
+    return data.logGroups.map(({ logGroupName }) => logGroupName)
+  }
 
-        let data = await cw.describeLogGroups(params).promise();
-        return data.logGroups.map(({logGroupName})=>logGroupName);
-    }
-
-    private async runQuery(cw: AWS.CloudWatchLogs, start: Date, end: Date, groupNames: string[]): Promise<string>{
-        const query = `
+  private async runQuery(cw: AWS.CloudWatchLogs, start: Date, end: Date, groupNames: string[]): Promise<string> {
+    const query = `
             filter @type = "REPORT"
             | fields datefloor(@timestamp, 1d) as Date, @duration/1000 as DurationInS, @memorySize/1000000 as MemorySetInMB, 2.35 * DurationInS/3600 * MemorySetInMB/1792 as wattsPerFunction
             | stats sum(wattsPerFunction) as Watts by Date 
-            | sort Date asc`;
+            | sort Date asc`
 
-        let params = {
-            startTime: start.getTime(), /* required */
-            endTime: end.getTime(), /* required */
-            queryString: query, /* required */
-            logGroupNames: groupNames
-        };
-        let data = await cw.startQuery(params).promise();
-        return data.queryId;
+    const params = {
+      startTime: start.getTime() /* required */,
+      endTime: end.getTime() /* required */,
+      queryString: query /* required */,
+      logGroupNames: groupNames,
     }
+    const data = await cw.startQuery(params).promise()
+    return data.queryId
+  }
 
-    private async getResults(cw: AWS.CloudWatchLogs, queryId: string){
-        var params = {
-            queryId: queryId /* required */
-        };
-        let status="";
-        let data;
-        while (status!=="Complete"){
-            await wait(1000);
-            data = await cw.getQueryResults(params).promise();
-            status = data.status;
-        }
-        return data;
+  private async getResults(cw: AWS.CloudWatchLogs, queryId: string) {
+    const params = {
+      queryId: queryId /* required */,
     }
+    let status = ''
+    let data
+    while (status !== 'Complete') {
+      await wait(1000)
+      data = await cw.getQueryResults(params).promise()
+      status = data.status
+    }
+    return data
+  }
 }
 
 async function wait(ms: number) {
-    return new Promise(resolve => {
-        setTimeout(resolve, ms);
-    });
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
 }
