2e6f215ed8a36d7c267561b568bf0385055f6a1d
megangoodling <megan.goodling@thoughtworks.com>
2020-08-17 13:06:36 -0400

    begin rds cost implementation | Kamil, Megan

--- server/test/unit/services/RDS.test.ts
+++ server/test/unit/services/RDS.test.ts
@@ -2,6 +2,7 @@ import RDS from '@services/RDS'
 import FootprintEstimate from '@domain/FootprintEstimate'
 import RDSStorage from '@services/RDSStorage'
 import RDSComputeService from '@services/RDSCompute'
+import Cost from '@domain/Cost'
 
 describe('RDS Service', function () {
   it('Combines the results from both the RDSCompute and RDSStorage services ', async () => {
@@ -48,6 +49,47 @@ describe('RDS Service', function () {
         co2e: 3,
         timestamp: new Date('2020-01-01T00:00:00.000Z'),
         wattHours: 5,
+      },
+    ])
+  })
+
+  it('combines the cost from RDS compute and RDS storage', async () => {
+    // given
+    const rdsComputeMockGetCost: jest.Mock<Promise<Cost[]>> = jest.fn()
+    const rdsComputeMock: RDSComputeService = new RDSComputeService()
+    rdsComputeMock.getCosts = rdsComputeMockGetCost
+    rdsComputeMockGetCost.mockResolvedValueOnce([
+      {
+        timestamp: new Date('2020-08-17'),
+        amount: 33.33,
+        currency: 'USD',
+      },
+    ])
+
+    const rdsStorageMockGetCost: jest.Mock<Promise<Cost[]>> = jest.fn()
+    const rdsStorageMock: RDSStorage = new RDSStorage()
+    rdsStorageMock.getCosts = rdsStorageMockGetCost
+    rdsStorageMockGetCost.mockResolvedValueOnce([
+      {
+        timestamp: new Date('2020-08-17'),
+        amount: 66.66,
+        currency: 'USD',
+      },
+    ])
+
+    const rdsService: RDS = new RDS(rdsComputeMock, rdsStorageMock)
+
+    // when
+    const res = await rdsService.getCosts(new Date('2020-08-17'), new Date('2020-08-17'), 'us-east-1')
+
+    // then
+    expect(rdsComputeMockGetCost).toBeCalledWith(new Date('2020-08-17'), new Date('2020-08-17'), 'us-east-1')
+    expect(rdsStorageMockGetCost).toBeCalledWith(new Date('2020-08-17'), new Date('2020-08-17'), 'us-east-1')
+    expect(res).toEqual([
+      {
+        timestamp: new Date('2020-08-17'),
+        amount: 99.99,
+        currency: 'USD',
       },
     ])
   })
