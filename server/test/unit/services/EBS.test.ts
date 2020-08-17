f529ffe971f998d8c745429a856bd7ce264f5e46
kmarkow <kamilmarkow@gmail.com>
2020-08-17 15:25:12 -0400

    Add builders module similar to fixtures Megan | Kamil

--- server/test/unit/services/EBS.test.ts
+++ server/test/unit/services/EBS.test.ts
@@ -4,7 +4,7 @@ import EBS from '@services/EBS'
 import { AWS_POWER_USAGE_EFFECTIVENESS, HDDCOEFFICIENT, SSDCOEFFICIENT } from '@domain/FootprintEstimationConstants'
 import { StorageEstimator } from '@domain/StorageEstimator'
 import { AWS_REGIONS } from '@services/AWSRegions'
-import { buildCostExplorerGetUsageResponse } from '../../fixtures/builders'
+import { buildCostExplorerGetUsageResponse } from '@builders'
 
 beforeAll(() => {
   AWSMock.setSDKInstance(AWS)
