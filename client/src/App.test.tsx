1bd49a3ae3d846447dea5c3c4c6f18cbb1f210ca
megangoodling <megan.goodling@thoughtworks.com>
2020-08-18 10:32:09 -0400

    change frontend to show emissions by service table with costs | Megan

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/AWS Emissions and Wattage/i);
+  const linkElement = getByText(/AWS Emissions and Wattage and Cost/i);
   expect(linkElement).toBeInTheDocument();
 });
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,62 +1,92 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import { Typography } from '@material-ui/core';
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
-    marginTop: '5em',
-    marginBottom: '5em'
+    marginTop: "5em",
+    marginBottom: "5em",
   },
   headerCells: {
-    backgroundColor: 'black',
-    color: 'lightGreen',
-    textEmphasis: 'bold',
+    backgroundColor: "black",
+    color: "lightGreen",
+    textEmphasis: "bold",
   },
   rowCells: {
-    backgroundColor: 'black',
-    color: 'white',
-  }
+    backgroundColor: "black",
+    color: "white",
+  },
 });
 
 interface TableProps {
-  data: any
+  data: any;
 }
 
-function SimpleTable(props: TableProps) {
+function EmissionsByServiceTable(props: TableProps) {
   const classes = useStyles();
-  if(props.data.length === 0) return null
+  if (props.data.length === 0) return null;
 
-  const header = props.data[0]
-  const rows = props.data.slice(1)
-  
-  return  (
+  const header = props.data[0];
+  const rows = props.data.slice(1);
+
+  return (
     <TableContainer>
       <Table className={classes.table} aria-label="simple table">
-        <TableHead >
+        <TableHead>
           <TableRow>
             {header.map((cell: string, idx: number) => (
               <TableCell key={idx} className={classes.headerCells}>
-
                 {cell}
               </TableCell>
             ))}
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row: string[]) => (            
-          <TableRow key={row[0]}>
-            {row.map((cell: string, idx: number) => (
-              <TableCell key={idx}>
+          {rows.map((row: string[]) => (
+            <TableRow key={row[0]}>
+              {row.map((cell: string, idx: number) => (
+                <TableCell key={idx}>{cell}</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+function EmissionsByDayAndServiceTable(props: TableProps) {
+  const classes = useStyles();
+  if (props.data.length === 0) return null;
+
+  const header = props.data[0];
+  const rows = props.data.slice(1);
+
+  return (
+    <TableContainer>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            {header.map((cell: string, idx: number) => (
+              <TableCell key={idx} className={classes.headerCells}>
                 {cell}
               </TableCell>
             ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row: string[]) => (
+            <TableRow key={row[0]}>
+              {row.map((cell: string, idx: number) => (
+                <TableCell key={idx}>{cell}</TableCell>
+              ))}
             </TableRow>
           ))}
         </TableBody>
@@ -65,20 +95,22 @@ function SimpleTable(props: TableProps) {
   );
 }
 function App() {
-  const [data, setData] = useState([])
+  const [
+    emissionsByDayAndServiceData,
+    setEmissionsByDayAndServiceData,
+  ] = useState([]);
+  const [emissionsByServiceData, setEmissionsByServiceData] = useState([]);
 
   useEffect(() => {
-    fetch('/api')
-      .then(response => response.json())
-      .then(response => setData(response))
+    fetch("/api")
+      .then((response) => response.json())
+      .then((response) => setEmissionsByServiceData(response));
   }, []);
-  
+
   return (
     <div>
-      <Typography variant="h1">
-        AWS Emissions and Wattage
-      </Typography>
-      <SimpleTable data={data}/>
+      <Typography variant="h1">AWS Emissions and Wattage and Cost</Typography>
+      <EmissionsByServiceTable data={emissionsByServiceData} />
     </div>
   );
 }
