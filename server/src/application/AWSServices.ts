e4ef967680564177e2dfad63ce81e7a925d94aa7
Zama <jose.zamudio@thoughtworks.com>
2020-08-20 11:28:18 -0500

    Only services listed in Config.json are enabled | Jose

--- server/src/application/AWSServices.ts
+++ server/src/application/AWSServices.ts
@@ -7,14 +7,36 @@ import RDS from '@services/RDS'
 import RDSComputeService from '@services/RDSCompute'
 import RDSStorage from '@services/RDSStorage'
 import Lambda from '@services/Lambda'
+import { CURRENT_SERVICES } from '@application/Config.json'
 
 export default function AWSServices(): ICloudService[] {
-  return [
-    new EBS(),
-    new S3(),
-    new EC2(),
-    new ElastiCache(),
-    new RDS(new RDSComputeService(), new RDSStorage()),
-    new Lambda(),
-  ]
+  return CURRENT_SERVICES.map(({ key }) => {
+    return getService(key)
+  })
+}
+
+function getService(key: string): ICloudService {
+  if (services[key] === undefined) throw new Error('Unsupported service: ' + key)
+  return services[key]()
+}
+
+const services: { [id: string]: () => ICloudService } = {
+  ebs: () => {
+    return new EBS()
+  },
+  s3: () => {
+    return new S3()
+  },
+  ec2: () => {
+    return new EC2()
+  },
+  elasticache: () => {
+    return new ElastiCache()
+  },
+  rds: () => {
+    return new RDS(new RDSComputeService(), new RDSStorage())
+  },
+  lambda: () => {
+    return new Lambda()
+  },
 }
