da5c88242033a5d04b01951effc17edc1e2d03c0
vivian <vivian.liao@thoughtworks.com>
2020-08-28 12:06:18 -0700

    update tooltip, axis titles, and line styles | Vivian

--- client/src/ApexLineChart.tsx
+++ client/src/ApexLineChart.tsx
@@ -3,18 +3,55 @@ import Chart from 'react-apexcharts'
 import { transformData } from './transformData'
 import { EstimationResult } from './types'
 
-const options = {
-  chart: {
-    id: 'apexchart-example',
-  },
-  xaxis: {
-    type: 'datetime',
-  },
-}
-
-export const ApexLineChart: FunctionComponent<ApexLineChartProps> = ({ data }) => {
-  const timeSeriesData = transformData(data)
-  return <Chart options={options} series={timeSeriesData} type="line" height={320} />
+export const ApexLineChart: FunctionComponent<ApexLineChartProps> = ({data}) => {
+    const timeSeriesData = transformData(data)
+    const options = {
+        series: [{
+            name: 'co2e',
+            data: timeSeriesData
+        }],
+        chart: {
+            id: 'apexchart-example',
+        },
+        height: '500px',
+        legend: {
+          show: true
+        },
+        markers: {
+            size: 5,
+        },
+        stroke: {
+            width: 2
+        },
+        title: {
+            text: "Cloud Emissions",
+            style: {
+                fontSize: '24px'
+            },
+        },
+        xaxis: {
+            type: 'datetime',
+            title: {
+                text: 'Date',
+                offsetY: 10,
+                style: {
+                    fontSize: '15px'
+                }
+            }
+        },
+        yaxis:{
+            title: {
+                text: 'Daily co2e',
+                offsetX: -5,
+                style: {
+                    fontSize: '15px'
+                }
+            }
+        },
+    }
+    return (
+        <Chart options={options} series={options.series} type='line' height={options.height}/>
+    )
 }
 
 type ApexLineChartProps = {
--- client/src/transformData.tsx
+++ client/src/transformData.tsx
@@ -1,17 +1,16 @@
 import { EstimationResult, co2PerDay } from './types'
 
-const transformData = (data: EstimationResult[]): ApexAxisChartSeries => {
-  let returnData: co2PerDay[] = []
+const transformData = (data:EstimationResult[]):{}[] => {
+    let co2DailyTotals: co2PerDay[] = []
 
-  data.forEach((EstimationResult) => {
-    let co2eTotal = 0
-    EstimationResult.serviceEstimates.forEach((serviceEstimate) => {
-      co2eTotal += serviceEstimate.co2e
+    data.forEach((EstimationResult) => {
+        let co2eTotal = 0
+        EstimationResult.serviceEstimates.forEach((serviceEstimate) => {
+            co2eTotal += serviceEstimate.co2e
+        })
+        co2DailyTotals.push({x: EstimationResult.timestamp, y: co2eTotal})
     })
-    returnData.push({ x: EstimationResult.timestamp, y: co2eTotal })
-  })
-
-  return [{ data: returnData }]
+    return co2DailyTotals
 }
 
 export { transformData }
