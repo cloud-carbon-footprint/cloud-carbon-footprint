aac4858e77f5709f93e2fd97d6dd68e13cd8dad2
Omar Sulehria <osulehri@thoughtworks.com>
2020-09-02 11:54:45 -0600

    Use Material UI icons in line chart | Omar

--- client/src/ApexLineChart.tsx
+++ client/src/ApexLineChart.tsx
@@ -1,26 +1,37 @@
 import React, { FunctionComponent } from 'react'
+import { renderToStaticMarkup } from 'react-dom/server'
 import Chart from 'react-apexcharts'
+import { GetApp, PanTool, RotateLeft, ZoomIn } from '@material-ui/icons'
 import moment from 'moment'
 
 import { transformData } from './transformData'
 import { EstimationResult } from './types'
 
 export const ApexLineChart: FunctionComponent<ApexLineChartProps> = ({ data }) => {
+  // We need to get the HTML string version of these icons since ApexCharts doesn't take in custom React components.
+  // Why, you might ask? Don't ask me, ask ApexCharts.
+  const GetAppIconHTML = renderToStaticMarkup(<GetApp />)
+  const PanToolIconHTML = renderToStaticMarkup(<PanTool />)
+  const RotateLeftIconHTML = renderToStaticMarkup(<RotateLeft />)
+  const ZoomInIconHTML = renderToStaticMarkup(<ZoomIn />)
+
   const timeSeriesData = transformData(data)
   const options = {
     series: [
       {
-        name: 'co2e',
+        name: 'CO2e',
         data: timeSeriesData,
       },
     ],
     chart: {
-      id: 'apexchart-example',
       toolbar: {
         tools: {
+          download: GetAppIconHTML,
+          zoom: ZoomInIconHTML,
           zoomin: false,
           zoomout: false,
-          customIcons: [],
+          pan: PanToolIconHTML,
+          reset: RotateLeftIconHTML,
         },
       },
     },
