b371a88605471ad2e5d5b0ec0e4e494003acf2a7
austin-garrard <austin.w.garrard@gmail.com>
2020-09-10 15:56:13 -0500

    Refactor service filter | Austin

--- client/src/services.ts
+++ client/src/services.ts
@@ -9,9 +9,8 @@ export const SERVICE_OPTIONS: ServiceOption[] = [
   { key: ALL_SERVICES, name: 'All Services' },
   ...Config.CURRENT_SERVICES,
 ]
-export const NUM_SERVICES = Config.CURRENT_SERVICES.length
 
-export const serviceLabels: { [key: string]: string } = SERVICE_OPTIONS.reduce(
+export const SERVICE_LABELS: { [key: string]: string } = SERVICE_OPTIONS.reduce(
   (serviceOptions, service) => ({ ...serviceOptions, [service.key]: service.name }),
   {},
 )
