9029aa96465688fdf3ab688ba80f428e7803f47e
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2020-09-25 16:37:22 -0500

    Added Carbon Formula Drawer | Dan

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,30 @@
 import React from 'react'
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles'
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
 import CloudCarbonContainer from './dashboard/CloudCarbonContainer'
+import { CarbonFormulaDrawer } from './dashboard/CarbonFormulaDrawer'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+    },
+  }),
+)
 
 function App() {
+  const classes = useStyles()
+
   return (
     <>
-      <AppBar position="sticky" square={true}>
+      <AppBar position="sticky" square={true} className={classes.appBar}>
         <Toolbar>
           <Typography component="h1" variant="h5">
             Cloud Carbon Footprint
           </Typography>
         </Toolbar>
       </AppBar>
+      <CarbonFormulaDrawer />
       <Container maxWidth={'xl'}>
         <CloudCarbonContainer />
       </Container>
---
+++ client/src/dashboard/CarbonFormulaDrawer.tsx
@@ -0,0 +1,137 @@
+import React, { FunctionComponent, useState } from 'react'
+import clsx from 'clsx'
+import { createStyles, makeStyles } from '@material-ui/core/styles'
+import Drawer from '@material-ui/core/Drawer'
+import Divider from '@material-ui/core/Divider'
+import IconButton from '@material-ui/core/IconButton'
+import CloseIcon from '@material-ui/icons/Close'
+import InfoIcon from '@material-ui/icons/Info'
+import { Typography } from '@material-ui/core'
+
+const drawerWidth = 340
+
+const useStyles = makeStyles(({ palette, transitions, spacing, typography, breakpoints, mixins }) =>
+  createStyles({
+    infoButton: {
+      margin: spacing(10, 'auto'),
+      color: palette.type === 'dark' ? palette.text.primary : palette.primary.main,
+    },
+    closeButtonContainer: {
+      textAlign: 'right',
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+      whiteSpace: 'nowrap',
+    },
+    drawerOpen: {
+      width: drawerWidth,
+      transition: transitions.create('width', {
+        easing: transitions.easing.sharp,
+        duration: transitions.duration.enteringScreen,
+      }),
+    },
+    drawerClose: {
+      transition: transitions.create('width', {
+        easing: transitions.easing.sharp,
+        duration: transitions.duration.leavingScreen,
+      }),
+      overflowX: 'hidden',
+      width: spacing(4) + 1,
+      [breakpoints.up('sm')]: {
+        width: spacing(6) + 1,
+      },
+    },
+    toolbar: {
+      padding: spacing(10, 1),
+      // necessary for content to be below app bar
+      ...mixins.toolbar,
+    },
+    hide: {
+      display: 'none',
+    },
+    contentTitle: {
+      padding: spacing(2),
+      fontWeight: typography.fontWeightBold,
+    },
+    contentBold: {
+      padding: spacing(2, 2, 0, 2),
+      fontWeight: typography.fontWeightBold,
+    },
+    content: {
+      padding: spacing(2),
+      whiteSpace: 'pre-line',
+      fontSize: typography.body2.fontSize,
+    },
+  }),
+)
+
+export const CarbonFormulaDrawer: FunctionComponent = () => {
+  const classes = useStyles()
+  const [open, setOpen] = useState(false)
+
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <Drawer
+      anchor="right"
+      variant="permanent"
+      className={clsx(classes.drawer, {
+        [classes.drawerOpen]: open,
+        [classes.drawerClose]: !open,
+      })}
+      classes={{
+        paper: clsx({
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        }),
+      }}
+    >
+      <IconButton
+        color="inherit"
+        aria-label="open drawer"
+        onClick={handleDrawerOpen}
+        edge="start"
+        className={clsx(classes.infoButton, {
+          [classes.hide]: open,
+        })}
+      >
+        <InfoIcon data-testid="infoIcon" />
+      </IconButton>
+      <div
+        className={clsx(classes.toolbar, {
+          [classes.hide]: !open,
+        })}
+      >
+        <div className={classes.closeButtonContainer}>
+          <IconButton onClick={handleDrawerClose}>
+            <CloseIcon data-testid="closeIcon" />
+          </IconButton>
+        </div>
+        <Typography className={classes.contentTitle} component="p">
+          How do we get our carbon estimates?
+        </Typography>
+        <Divider />
+        <Typography className={classes.contentBold} component="p">
+          Our CO2e Estimate Formula:
+        </Typography>
+        <Typography className={classes.content} component="p">
+          (Cloud provider service usage) x (Cloud provider Power Usage Effectiveness [PUE]) x (Cloud energy conversion
+          factors [Wh]) x (EPA [US] or carbonfootprint.com [Non-US] grid emissions factors [CO2e])
+        </Typography>
+        <Divider />
+        <Typography className={classes.content} component="p">
+          Currently we estimate CO2e emissions for cloud compute and storage services. Networking and memory services
+          usage are not estimated yet due to their comparatively small footprint and current lack of available energy
+          conversion factors.
+        </Typography>
+      </div>
+    </Drawer>
+  )
+}
