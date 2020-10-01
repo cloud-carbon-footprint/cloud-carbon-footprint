5bd4d908c5e1c29ea4c5c2246b9befaecdc17f2b
austin-garrard <austin.w.garrard@gmail.com>
2020-10-01 17:19:09 -0500

    cleanup

--- appengine/deploy.sh
+++ appengine/deploy.sh
@@ -3,6 +3,8 @@
 # Requires SERVER_VERSION and CLIENT_VERSION to be set to valid tags
 # for their respective image repository
 
+set -x
+
 gcloud --quiet app deploy \
   --image-url=gcr.io/cloud-carbon-footprint/server:${SERVER_VERSION} \
   server.yaml
--- terraform/config.tf
+++ terraform/config.tf
@@ -10,6 +10,7 @@ provider "google" {
    }
  }
 
+# this was imported, so the location_id was already set and we can't change it
 resource "google_app_engine_application" "app" {
   project     = var.project_id
   location_id = "us-west2"
