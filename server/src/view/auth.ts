ec62807fcac07bcb88d1018cc5c6d290affb9485
austin-garrard <austin.w.garrard@gmail.com>
2020-10-02 09:18:41 -0500

    refactor server towards auth

---
+++ server/src/view/auth.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+
+export default async function (req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> {
+  try {
+    // https://github.com/okta/okta-oidc-js/tree/master/packages/jwt-verifier
+    // const accessTokenString = req.get('Authorization')
+    // const verifier = new OktaJwtVerifier({
+    //   issuer: process.env.OKTA_ISSUER,
+    //   clientId: process.env.OKTA_CLIENT_ID,
+    // })
+    // const accessToken = verifier.verifyAccessToken(accessTokenString, 'api://default')
+    // res.locals.userInfo = accessToken.claims
+    console.log('Auth successful')
+    next()
+  } catch (e) {
+    res.status(401).send('Unauthorized')
+  }
+}
--- server/src/view/server.ts
+++ server/src/view/server.ts
@@ -1,8 +1,18 @@
+import express from 'express'
+import api from './api'
+import auth from './auth'
+
 if (process.env.NODE_ENV === 'production') {
   require('module-alias/register')
 }
 
 const port = process.env.PORT || 4000
-import httpApp from '@view/api'
+const httpApp = express()
+
+if (process.env.NODE_ENV === 'production') {
+  httpApp.use(auth)
+}
+
+httpApp.use('/api', api)
 
 httpApp.listen(port, () => console.log(`Cloud Carbon Footprint Server listening at http://localhost:${port}`))
