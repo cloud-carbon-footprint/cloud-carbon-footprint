dfe4ffa79a57dac72eef04bf930e086ace5e0d87
tomshannon1 <tshannon@carthage.edu>
2020-10-06 16:04:07 -0500

    Experimenting mocks for compute engine test | Tim, Tom, Vivian

--- server/src/services/gcp/ComputeEngine.ts
+++ server/src/services/gcp/ComputeEngine.ts
@@ -1,6 +1,10 @@
 import ServiceWithCPUUtilization from '@domain/ServiceWithCPUUtilization'
 import ComputeUsage from '@domain/ComputeUsage'
 import Cost from '@domain/Cost'
+import { MetricServiceClient } from "@google-cloud/monitoring" 
+import {google} from '@google-cloud/monitoring/build/protos/protos'
+import Aligner = google.monitoring.v3.Aggregation.Aligner
+import Reducer = google.monitoring.v3.Aggregation.Reducer
 
 export default class ComputeEngine extends ServiceWithCPUUtilization {
   serviceName = 'computeEngine'
@@ -9,9 +13,40 @@ export default class ComputeEngine extends ServiceWithCPUUtilization {
     super()
   }
 
-  async getUsage(start: Date, end: Date, region: string): Promise<ComputeUsage[]> {
+  async getUsage(start: Date, end: Date, region: string): Promise<any[]> {
+    let client = new MetricServiceClient();
+
+    const startTime = '2020-09-16T00:59:00.000Z';
+    const endTime = '2020-09-17T00:00:00.000Z';
+  
+    const projectId = 'cloud-carbon-footprint';
+
+    const request = {
+      name: client.projectPath(projectId),
+      filter: 'resource.type = "gce_instance" AND ' +
+        'metric.type="compute.googleapis.com/instance/cpu/utilization" AND ' +
+        'metadata.system_labels.region="us-central1"',
+      aggregation: {
+        //one hour alignment
+        alignmentPeriod: {seconds: 3600},
+        perSeriesAligner: Aligner.ALIGN_MEAN, //average for the instance, in an hour
+        crossSeriesReducer: Reducer.REDUCE_MEAN //aggregate the mean across instances
+      },
+      interval: {
+        startTime: {
+          seconds: 1000,
+        },
+        endTime: {
+          seconds: 1000,
+        },
+      },
+    };
+
+
+    const [timeSeries] = await client.listTimeSeries(request)
+    console.log(timeSeries)
     console.log(`getUsage not Implemented. Called with start: ${start}, end: ${end}, region: ${region}`)
-    return []
+    return [timeSeries]
   }
 
   async getCosts(start: Date, end: Date, region: string): Promise<Cost[]> {
---
+++ server/test/unit/services/aws/RDS.test.ts
@@ -0,0 +1,148 @@
+import RDS from '@services/aws/RDS'
+import FootprintEstimate from '@domain/FootprintEstimate'
+import RDSStorage from '@services/aws/RDSStorage'
+import RDSComputeService from '@services/aws/RDSCompute'
+import Cost from '@domain/Cost'
+import { ServiceWrapper } from '@services/aws/ServiceWrapper'
+import { CloudWatch, CostExplorer } from 'aws-sdk'
+
+describe('RDS Service', function () {
+  const startDate = '2020-08-16'
+  const endDate = '2020-08-17'
+  const region = 'us-east-1'
+
+  it('Combines the results from both the RDSCompute and RDSStorage services ', async () => {
+    const rdsComputeEstimate: FootprintEstimate[] = [
+      {
+        co2e: 1,
+        timestamp: new Date(startDate),
+        wattHours: 4,
+      },
+    ]
+
+    const rdsStorageEstimate: FootprintEstimate[] = [
+      {
+        co2e: 2,
+        timestamp: new Date(startDate),
+        wattHours: 1,
+      },
+    ]
+
+    const rdsComputeMockGetEstimates: jest.Mock<Promise<FootprintEstimate[]>> = jest.fn()
+    const rdsComputeMock: RDSComputeService = new RDSComputeService(
+      new ServiceWrapper(new CloudWatch(), new CostExplorer()),
+    )
+    rdsComputeMock.getEstimates = rdsComputeMockGetEstimates
+    rdsComputeMockGetEstimates.mockResolvedValueOnce(rdsComputeEstimate)
+
+    const rdsStorageMockGetEstimates: jest.Mock<Promise<FootprintEstimate[]>> = jest.fn()
+    const rdsStorageMock: RDSStorage = new RDSStorage(new ServiceWrapper(new CloudWatch(), new CostExplorer()))
+    rdsStorageMock.getEstimates = rdsStorageMockGetEstimates
+    rdsStorageMockGetEstimates.mockResolvedValueOnce(rdsStorageEstimate)
+
+    const rdsService: RDS = new RDS(rdsComputeMock, rdsStorageMock)
+    const rdsEstimates = await rdsService.getEstimates(new Date(startDate), new Date(endDate), region)
+
+    expect(rdsEstimates).toEqual([
+      {
+        co2e: 3,
+        timestamp: new Date(startDate),
+        wattHours: 5,
+      },
+    ])
+  })
+
+  it('combines the cost from RDS compute and RDS storage', async () => {
+    const rdsComputeMockGetCost: jest.Mock<Promise<Cost[]>> = jest.fn()
+    const rdsComputeMock: RDSComputeService = new RDSComputeService(
+      new ServiceWrapper(new CloudWatch(), new CostExplorer()),
+    )
+    rdsComputeMock.getCosts = rdsComputeMockGetCost
+    rdsComputeMockGetCost.mockResolvedValueOnce([
+      {
+        timestamp: new Date(startDate),
+        amount: 33.33,
+        currency: 'USD',
+      },
+    ])
+
+    const rdsStorageMockGetCost: jest.Mock<Promise<Cost[]>> = jest.fn()
+    const rdsStorageMock: RDSStorage = new RDSStorage(new ServiceWrapper(new CloudWatch(), new CostExplorer()))
+    rdsStorageMock.getCosts = rdsStorageMockGetCost
+    rdsStorageMockGetCost.mockResolvedValueOnce([
+      {
+        timestamp: new Date(startDate),
+        amount: 66.66,
+        currency: 'USD',
+      },
+    ])
+
+    const rdsService: RDS = new RDS(rdsComputeMock, rdsStorageMock)
+    const res = await rdsService.getCosts(new Date(startDate), new Date(startDate), region)
+
+    expect(rdsComputeMockGetCost).toBeCalledWith(new Date(startDate), new Date(startDate), region)
+    expect(rdsStorageMockGetCost).toBeCalledWith(new Date(startDate), new Date(startDate), region)
+    expect(res).toEqual([
+      {
+        timestamp: new Date(startDate),
+        amount: 99.99,
+        currency: 'USD',
+      },
+    ])
+  })
+
+  it('combines the cost from RDS compute and RDS storage for 2 days', async () => {
+    const rdsComputeMockGetCost: jest.Mock<Promise<Cost[]>> = jest.fn()
+    const rdsComputeMock: RDSComputeService = new RDSComputeService(
+      new ServiceWrapper(new CloudWatch(), new CostExplorer()),
+    )
+    rdsComputeMock.getCosts = rdsComputeMockGetCost
+    rdsComputeMockGetCost.mockResolvedValueOnce([
+      {
+        timestamp: new Date(startDate),
+        amount: 33.33,
+        currency: 'USD',
+      },
+      {
+        timestamp: new Date(endDate),
+        amount: 11.11,
+        currency: 'USD',
+      },
+    ])
+
+    const rdsStorageMockGetCost: jest.Mock<Promise<Cost[]>> = jest.fn()
+    const rdsStorageMock: RDSStorage = new RDSStorage(new ServiceWrapper(new CloudWatch(), new CostExplorer()))
+    rdsStorageMock.getCosts = rdsStorageMockGetCost
+    rdsStorageMockGetCost.mockResolvedValueOnce([
+      {
+        timestamp: new Date(startDate),
+        amount: 66.66,
+        currency: 'USD',
+      },
+      {
+        timestamp: new Date(endDate),
+        amount: 22.22,
+        currency: 'USD',
+      },
+    ])
+
+    const rdsService: RDS = new RDS(rdsComputeMock, rdsStorageMock)
+
+    const res = await rdsService.getCosts(new Date(startDate), new Date(endDate), region)
+
+    expect(rdsComputeMockGetCost).toBeCalledWith(new Date(startDate), new Date(endDate), region)
+    expect(rdsStorageMockGetCost).toBeCalledWith(new Date(startDate), new Date(endDate), region)
+    expect(res).toEqual([
+      {
+        timestamp: new Date(startDate),
+        amount: 99.99,
+        currency: 'USD',
+      },
+      {
+        timestamp: new Date(endDate),
+        amount: 33.33,
+        currency: 'USD',
+      },
+    ])
+  })
+})
