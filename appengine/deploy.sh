a6df3d1892fb1d7bf82fd4daa76933faa6ef6a67
vivian <vivian.liao@thoughtworks.com>
2020-10-12 15:00:47 -0700

    pin version of client and server appengine deployments to staging | Vivian

--- appengine/deploy.sh
+++ appengine/deploy.sh
@@ -7,9 +7,11 @@ set -x
 
 gcloud --quiet app deploy \
   --image-url=gcr.io/cloud-carbon-footprint/server:${SERVER_VERSION} \
+  --version=staging \
   server.yaml
 gcloud --quiet app deploy \
   --image-url=gcr.io/cloud-carbon-footprint/client:${CLIENT_VERSION} \
+  --version=staging \
   client.yaml
 gcloud --quiet app deploy \
   dispatch.yaml
