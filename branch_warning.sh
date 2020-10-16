45628a88d2fd6cfc80b9067751124992e199b28c
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2020-10-16 12:09:41 -0500

    added bach script and pre-commit hook for confirming that you want to commit to a non-trunk branch | Dan

---
+++ branch_warning.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+branch="$(git rev-parse --abbrev-ref HEAD)"
+
+if [[ "$branch" != "trunk" ]]; then
+    read -p "Are you sure you want to use a branch that isn't trunk? (y/n): " -n 1 -r < /dev/tty
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo "Okie dokie. Be Careful out there :)"
+        exit 0
+    fi
+
+    echo "Commit aborted. Head on back to the trunk :)"
+    exit 1
+fi
