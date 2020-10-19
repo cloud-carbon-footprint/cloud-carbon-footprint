a654d3cc58f6f879a9cee686379b2a84f1707970
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2020-10-19 17:49:58 -0500

    added script to generate client .env file | Dan

---
+++ client/create_client_env_file.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo HOST_URL=$HOST_URL >> .env
+echo OKTA_ORG_URL=$OKTA_ORG_URL >> .env
+echo APP_SECRET=$APP_SECRET >> .env
+echo OKTA_CLIENT_ID=$OKTA_CLIENT_ID >> .env
+echo OKTA_CLIENT_SECRET=$OKTA_CLIENT_SECRET >> .env
