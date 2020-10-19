695b06fd422934ef21a0bedd3367a0371bceb576
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2020-10-19 17:04:07 -0500

    fixed Okta Auth implementation | Dan

--- client/static-server/index.js
+++ client/static-server/index.js
@@ -6,6 +6,15 @@ require('dotenv').config()
 const port = process.env.PORT || 8080
 const app = express()
 
+const oidc = new ExpressOIDC({
+  appBaseUrl: process.env.HOST_URL,
+  issuer: `${process.env.OKTA_ORG_URL}`,
+  client_id: process.env.OKTA_CLIENT_ID,
+  client_secret: process.env.OKTA_CLIENT_SECRET,
+  redirect_uri: `${process.env.HOST_URL}/authorization-code/callback`,
+  scope: 'openid profile',
+})
+
 app.use(
   session({
     secret: process.env.APP_SECRET,
@@ -14,35 +23,21 @@ app.use(
   }),
 )
 
-const oidc = new ExpressOIDC({
-  appBaseUrl: process.env.HOST_URL,
-  issuer: `${process.env.OKTA_ORG_URL}`,
-  client_id: process.env.OKTA_CLIENT_ID,
-  client_secret: process.env.OKTA_CLIENT_SECRET,
-  redirect_uri: `${process.env.HOST_URL}/callback`,
-  scope: 'openid profile',
-  routes: {
-    loginCallback: {
-      path: '/callback',
-    },
-  },
-})
-
 app.use(oidc.router)
 
-//redirect any unauthenticated requests to any path to /login, OIDC is listening for requests to login
-app.use(oidc.ensureAuthenticated(), (req, res) => {
-  res.redirect('/login')
-});
+app.use(oidc.ensureAuthenticated(), (req, res, next) => {
+  next()
+})
 
 app.use(express.static('build'))
 
 oidc.on('ready', () => {
   app.listen(port, () => console.log(`Cloud Carbon Footprint Server listening at http://localhost:${port}`))
 })
-oidc.on('error', err => {
+
+oidc.on('error', (err) => {
   // An error occurred while setting up OIDC, during token revokation, or during post-logout handling
-  console.log("an error occurred", err)
+  console.log('An error occurred', err)
 })
 
 module.exports = app
