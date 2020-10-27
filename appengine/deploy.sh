6dcc6011ac6c197c7852195af5367db34278f75a
dtoakley-tw <69360383+dtoakley-tw@users.noreply.github.com>
2020-10-27 15:35:25 -0500

    Remove docker Container support for the CircleCI Pipeline (#7)
    * removed support for Docker Containers from CircleCI build pipeline. Refactored App Engine Config to support Standard Environment | Dan
    
    * Fixed CircleCI config errors | Dan
    
    * fixed deploy script | Dan
    
    * updated circleci config to attached workspace reference to jobs | Dan, Maurice
    
    * added persist_to_workspace for built assets | Dan
    
    * fixed persist_to_workspace for built assets | Dan
    
    Co-authored-by: Dan Lewis-Toakley <d.lewistoakley@gmail.com>

--- appengine/deploy.sh
+++ appengine/deploy.sh
@@ -4,18 +4,13 @@
 # © 2020 ThoughtWorks, Inc. All rights reserved.
 #
 
-# Requires SERVER_VERSION and CLIENT_VERSION to be set to valid tags
-# for their respective image repository
-
 set -x
 
 gcloud --quiet app deploy \
-  --image-url=gcr.io/cloud-carbon-footprint/server:${SERVER_VERSION} \
-  --version=stagingflex \
-  server.yaml
+  --version=staging \
+  ../server/app.yaml
 gcloud --quiet app deploy \
-  --image-url=gcr.io/cloud-carbon-footprint/client:${CLIENT_VERSION} \
-  --version=stagingflex \
-  client.yaml
+  --version=staging \
+  ../client/app.yaml
 gcloud --quiet app deploy \
   dispatch.yaml
