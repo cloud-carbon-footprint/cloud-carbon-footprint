498c9018652a58c81e02bba7be0c4a772dbde2ee
scappellotw <stephanie.cappello@thoughtworks.com>
2020-10-28 12:35:02 -0500

    Precommit for checking copyright on typescript files

---
+++ copyright_check.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#
+# © 2020 ThoughtWorks, Inc. All rights reserved.
+#
+
+copyright="© 2020 ThoughtWorks, Inc. All rights reserved."
+errorCode=0
+git diff --cached --name-status | while read x file; do
+  if [ "$x" == 'D' ]; then continue; fi
+  holder=$(echo $file | grep ".*\.ts" | wc -l)
+  if [ $holder -eq 0 ]; then continue; fi
+  if grep "$copyright" $file; then continue; fi
+
+  echo "ERROR: \"$copyright\" not in file: ${file}"
+  exit 1
+done || exit 1
