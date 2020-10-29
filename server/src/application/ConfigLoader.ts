1c0e3f5a86ef0c04484c25b44a61c5af84aa0e83
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2020-10-29 15:21:21 -0500

    Adds a GCP specific Logger and updates the tests | Dan

--- server/src/application/ConfigLoader.ts
+++ server/src/application/ConfigLoader.ts
@@ -2,29 +2,8 @@
  * © 2020 ThoughtWorks, Inc. All rights reserved.
  */
 
-import configFile from '@application/Config'
+import configFile, { CCFConfig } from '@application/Config'
 
 export default function config(): CCFConfig {
   return configFile
-}
-
-interface CCFConfig {
-  AWS?: {
-    NAME: string
-    CURRENT_SERVICES: { key: string; name: string }[]
-    CURRENT_REGIONS: string[]
-    accounts?: {
-      id: string
-      name?: string
-    }[]
-    authentication?: {
-      mode: string
-      options?: Record<string, string>
-    }
-  }
-  GCP?: {
-    NAME: string
-    CURRENT_SERVICES: { key: string; name: string }[]
-    CURRENT_REGIONS: string[]
-  }
 }
