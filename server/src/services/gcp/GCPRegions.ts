d8f0bce335aa7731b965a8d8ac6251e074f43842
scappellotw <stephanie.cappello@thoughtworks.com>
2020-11-16 12:45:50 -0600

    [805] Add GCP Regions and their Co2e coefficients|Dan, Steph

--- server/src/services/gcp/GCPRegions.ts
+++ server/src/services/gcp/GCPRegions.ts
@@ -2,9 +2,29 @@
  * © 2020 ThoughtWorks, Inc. All rights reserved.
  */
 
-// TODO - Update this with the complete list of regions.
 export enum GCP_REGIONS {
-  US_EAST1 = 'us-east1',
-  US_WEST1 = 'us-west1',
+  ASIA_EAST1 = 'asia-east1',
+  ASIA_EAST2 = 'asia-east2',
+  ASIA_NORTHEAST1 = 'asia-northeast1',
+  ASIA_NORTHEAST2 = 'asia-northeast2',
+  ASIA_NORTHEAST3 = 'asia-northeast3',
+  ASIA_SOUTH1 = 'asia-south1',
+  ASIA_SOUTHEAST1 = 'asia-southeast1',
+  ASIA_SOUTHEAST2 = 'asia-southeast2',
+  AUSTRALIA_SOUTHEAST1 = 'australia-southeast1',
+  EUROPE_NORTH1 = 'europe-north1',
+  EUROPE_WEST1 = 'europe-west1',
+  EUROPE_WEST2 = 'europe-west2',
+  EUROPE_WEST3 = 'europe-west3',
+  EUROPE_WEST4 = 'europe-west4',
+  EUROPE_WEST6 = 'europe-west6',
+  NORTHAMERICA_NORTHEAST1 = 'northamerica-northeast1',
+  SOUTHAMERICA_EAST1 = 'southamerica-east1',
   US_CENTRAL1 = 'us-central1',
+  US_EAST1 = 'us-east1',
+  US_EAST4 = 'us-east4',
+  US_WEST1 = 'us-west1',
+  US_WEST2 = 'us-west2',
+  US_WEST3 = 'us-west3',
+  US_WEST4 = 'us-west4',
 }
