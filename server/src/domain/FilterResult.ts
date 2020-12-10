83f986e64b3b442ed3229972246044dda336ce31
scappellotw <stephanie.cappello@thoughtworks.com>
2020-12-10 15:42:24 -0600

    [716] adjust key to return instead of name on filters endpoint|Emily, Steph

--- server/src/domain/FilterResult.ts
+++ server/src/domain/FilterResult.ts
@@ -18,14 +18,14 @@ export const getAccounts = (): Account[] => {
   if (process.env.AWS_ACCOUNTS) {
     const awsAccounts: { id: string; name: string }[] = JSON.parse(process.env.AWS_ACCOUNTS)
     awsAccounts.forEach((awsAccount: { id: string; name: string }) => {
-      listOfAllAccounts.push({ cloudProvider: 'AWS', key: awsAccount.id, name: awsAccount.name })
+      listOfAllAccounts.push({ cloudProvider: 'aws', key: awsAccount.id, name: awsAccount.name })
     })
   }
 
   if (process.env.GCP_PROJECTS) {
     const gcpAccounts: { id: string; name: string }[] = JSON.parse(process.env.GCP_PROJECTS)
     gcpAccounts.forEach((gcpAccount: { id: string; name: string }) => {
-      listOfAllAccounts.push({ cloudProvider: 'GCP', key: gcpAccount.id, name: gcpAccount.name })
+      listOfAllAccounts.push({ cloudProvider: 'gcp', key: gcpAccount.id, name: gcpAccount.name })
     })
   }
 
--- server/test/unit/domain/FilterResult.test.ts
+++ server/test/unit/domain/FilterResult.test.ts
@@ -6,15 +6,15 @@ import { Account, getAccounts } from '@domain/FilterResult'
 
 describe('getFilterResults', () => {
   it('AWS results', () => {
-    const mockAwsAccount: Account = { cloudProvider: 'AWS', key: '123456678', name: 'Test Account' }
-    const mockGcpAccount: Account = { cloudProvider: 'GCP', key: '123456679', name: 'GCP Test Account' }
+    const mockAwsAccount: Account = { cloudProvider: 'aws', key: '123456678', name: 'Test Account' }
+    const mockGcpAccount: Account = { cloudProvider: 'gcp', key: '123456679', name: 'GCP Test Account' }
     const expectedArrayResults = [mockAwsAccount, mockGcpAccount]
 
     expect(expectedArrayResults).toEqual(getAccounts())
   })
   it('no GCP results', () => {
     delete process.env.GCP_PROJECTS
-    const mockAwsAccount: Account = { cloudProvider: 'AWS', key: '123456678', name: 'Test Account' }
+    const mockAwsAccount: Account = { cloudProvider: 'aws', key: '123456678', name: 'Test Account' }
     const expectedArrayResults = [mockAwsAccount]
 
     expect(expectedArrayResults).toEqual(getAccounts())
