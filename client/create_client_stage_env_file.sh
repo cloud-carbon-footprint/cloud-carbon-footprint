18939ab80939406d01f5291c8fb9bc9738986574
Cameron Casher <cameron.casher@thoughtworks.com>
2021-01-13 16:01:06 -0700

    [712] update production pipeline config | maurice, stephanie, cam

---
+++ client/create_client_stage_env_file.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# © 2020 ThoughtWorks, Inc. All rights reserved.
+#
+
+echo HOST_URL=$HOST_URL >> .env
+echo OKTA_ORG_URL=$OKTA_ORG_URL >> .env
+echo APP_SECRET=$APP_SECRET >> .env
+echo OKTA_CLIENT_ID=$OKTA_CLIENT_ID >> .env
+echo OKTA_CLIENT_SECRET=$OKTA_CLIENT_SECRET >> .env
+echo REACT_APP_AWS_USE_BILLING_DATA="true" >> .env
+echo REACT_APP_GCP_USE_BILLING_DATA="true" >> .env
