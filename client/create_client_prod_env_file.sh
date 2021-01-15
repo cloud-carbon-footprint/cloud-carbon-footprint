d797b608bcfd34e3f1f0af926a57e9970184162a
dtoakley-tw <69360383+dtoakley-tw@users.noreply.github.com>
2021-01-14 18:11:31 -0600

    Production pipeline (#8)
    * [712] Adds CircleCI config for production deployment | Dan, Maurice, Cam, Stephanie
    
    Co-authored-by: maurice <maurice.okumu@thoughtworks.com>
    Co-authored-by: Cameron Casher <cameron.casher@thoughtworks.com>
    Co-authored-by: Tim Brandle <tim.brandle@thoughtworks.com>
    Co-authored-by: Salvador Ariza <sariza@thoughtwork.com>
    Co-authored-by: Jonah Price <jonah.price@thoughtworks.com>

---
+++ client/create_client_prod_env_file.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#
+# © 2020 ThoughtWorks, Inc. All rights reserved.
+#
+
+rm .env
+echo HOST_URL=$PROD_HOST_URL >> .env
+echo OKTA_ORG_URL=$PROD_OKTA_ORG_URL >> .env
+echo APP_SECRET=$PROD_APP_SECRET >> .env
+echo OKTA_CLIENT_ID=$PROD_OKTA_CLIENT_ID >> .env
+echo OKTA_CLIENT_SECRET=$PROD_OKTA_CLIENT_SECRET >> .env
+echo REACT_APP_AWS_USE_BILLING_DATA="true" >> .env
+echo REACT_APP_GCP_USE_BILLING_DATA="true" >> .env
