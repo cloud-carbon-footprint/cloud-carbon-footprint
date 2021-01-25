4835bd151905c8bc6d318ed8cf3e23c86cd08b07
Salvador Ariza <sariza@thoughtwork.com>
2021-01-25 15:42:03 -0600

    type apex bar chart test data | Sal

--- client/src/dashboard/charts/ApexLineChart.test.tsx
+++ client/src/dashboard/charts/ApexLineChart.test.tsx
@@ -7,17 +7,19 @@ import { create } from 'react-test-renderer'
 import moment from 'moment'
 
 import { ApexLineChart } from './ApexLineChart'
+import { EstimationResult } from '../../models/types'
 
 jest.mock('../../themes')
 
 describe('ApexLineChart', () => {
   it('renders with correct configuration', () => {
-    const data = [
+    const data: EstimationResult[] = [
       {
         timestamp: moment('2019-08-10T00:00:00.000Z').toDate(),
         serviceEstimates: [
           {
-            timestamp: moment('2019-08-10T00:00:00.000Z').toDate(),
+            cloudProvider: 'AWS',
+            accountName: 'account-1',
             serviceName: 'ebs',
             wattHours: 0,
             co2e: 5,
@@ -25,7 +27,8 @@ describe('ApexLineChart', () => {
             region: 'us-west-2',
           },
           {
-            timestamp: moment('2019-08-10T00:00:00.000Z').toDate(),
+            cloudProvider: 'AWS',
+            accountName: 'account-1',
             serviceName: 's3',
             wattHours: 0,
             co2e: 0,
@@ -33,7 +36,8 @@ describe('ApexLineChart', () => {
             region: 'us-west-2',
           },
           {
-            timestamp: moment('2019-08-10T00:00:00.000Z').toDate(),
+            cloudProvider: 'AWS',
+            accountName: 'account-1',
             serviceName: 'ec2',
             wattHours: 0,
             co2e: 2,
