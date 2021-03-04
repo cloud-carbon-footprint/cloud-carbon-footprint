a4cbe4d2c87ad4438d8e6650052bbfdfeef446be
Cameron Casher <cameron.casher@thoughtworks.com>
2021-03-04 15:31:20 -0700

    [1068] adds create tag and release scripts

---
+++ scripts/create-github-release.js
@@ -0,0 +1,140 @@
+#!/usr/bin/env node
+/* eslint-disable import/no-extraneous-dependencies */
+
+const { Octokit } = require('@octokit/rest')
+
+// See Examples above to learn about these command line arguments.
+const [TAG_NAME, BOOL_CREATE_RELEASE] = process.argv.slice(2)
+
+if (!BOOL_CREATE_RELEASE) {
+  console.log(
+    '\nRunning script in Dry Run mode. It will output details, will create a draft release but will NOT publish it.',
+  )
+}
+
+const GH_OWNER = 'ThoughtWorks-Cleantech'
+const GH_REPO = 'cloud-carbon-footprint'
+const EXPECTED_COMMIT_MESSAGE = /^Merge pull request #(?<prNumber>[0-9]+) from/
+const CHANGESET_RELEASE_BRANCH = 'cloud-carbon-footprin/changeset-release/trunk'
+
+// Initialize a GitHub client
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+})
+
+// Get the message of the commit responsible for a tag
+async function getCommitMessageUsingTagName(tagName) {
+  // Get the tag SHA using the provided tag name
+  const refData = await octokit.git.getRef({
+    owner: GH_OWNER,
+    repo: GH_REPO,
+    ref: `tags/${tagName}`,
+  })
+  if (refData.status !== 200) {
+    console.error('refData:')
+    console.error(refData)
+    throw new Error('Something went wrong when getting the tag SHA using tag name')
+  }
+  const tagSha = refData.data.object.sha
+  console.log(`SHA for the tag ${TAG_NAME} is ${tagSha}`)
+
+  // Get the commit SHA using the tag SHA
+  const tagData = await octokit.git.getTag({
+    owner: GH_REPO,
+    repo: GH_REPO,
+    tag_sha: tagSha,
+  })
+  if (tagData.status !== 200) {
+    console.error('tagData:')
+    console.error(tagData)
+    throw new Error('Something went wrong when getting the commit SHA using tag SHA')
+  }
+  const commitSha = tagData.data.object.sha
+  console.log(`The commit for the tag is https://github.com/ThoughtWorks-Cleantech/backstage/commit/${commitSha}`)
+
+  // Get the commit message using the commit SHA
+  const commitData = await octokit.git.getCommit({
+    owner: GH_OWNER,
+    repo: GH_REPO,
+    commit_sha: commitSha,
+  })
+  if (commitData.status !== 200) {
+    console.error('commitData:')
+    console.error(commitData)
+    throw new Error('Something went wrong when getting the commit message using commit SHA')
+  }
+
+  // Example Commit Message
+  // Merge pull request #3555 from backstage/changeset-release/master Version Packages
+  return commitData.data.message
+}
+
+// There is a PR number in our expected commit message. Get the description of that PR.
+async function getReleaseDescriptionFromCommitMessage(commitMessage) {
+  // It should exactly match the pattern of changeset commit message, or else will abort.
+  const expectedMessage = RegExp(EXPECTED_COMMIT_MESSAGE)
+  if (!expectedMessage.test(commitMessage)) {
+    throw new Error(`Expected regex did not match commit message: ${commitMessage}`)
+  }
+
+  // Get the PR description from the commit message
+  const prNumber = commitMessage.match(expectedMessage).groups.prNumber
+  console.log(
+    `Identified the changeset Pull request - https://github.com/backstage/cloud-carbon-footprint/pull/${prNumber}`,
+  )
+
+  const { data } = await octokit.pulls.get({
+    owner: GH_OWNER,
+    repo: GH_REPO,
+    pull_number: prNumber,
+  })
+
+  // Use the PR description to prepare for the release description
+  const isChangesetRelease = commitMessage.includes(CHANGESET_RELEASE_BRANCH)
+  if (isChangesetRelease) {
+    return data.body.split('\n').slice(3).join('\n')
+  }
+
+  return data.body
+}
+
+// Create Release on GitHub.
+async function createRelease(releaseDescription) {
+  // Create draft release if BOOL_CREATE_RELEASE is undefined
+  // Publish release if BOOL_CREATE_RELEASE is not undefined
+  const boolCreateDraft = !BOOL_CREATE_RELEASE
+
+  const releaseResponse = await octokit.repos.createRelease({
+    owner: GH_REPO,
+    repo: GH_REPO,
+    tag_name: TAG_NAME,
+    name: TAG_NAME,
+    body: releaseDescription,
+    draft: boolCreateDraft,
+    prerelease: false,
+  })
+
+  if (releaseResponse.status === 201) {
+    if (boolCreateDraft) {
+      console.log('Created draft release! Click Publish to notify users.')
+    } else {
+      console.log('Published release!')
+    }
+    console.log(releaseResponse.data.html_url)
+  } else {
+    console.error(releaseResponse)
+    throw new Error('Something went wrong when creating the release.')
+  }
+}
+
+async function main() {
+  const commitMessage = await getCommitMessageUsingTagName(TAG_NAME)
+  const releaseDescription = await getReleaseDescriptionFromCommitMessage(commitMessage)
+
+  await createRelease(releaseDescription)
+}
+
+main().catch((error) => {
+  console.error(error.stack)
+  process.exit(1)
+})
---
+++ scripts/create-release-tag.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+/* eslint-disable import/no-extraneous-dependencies */
+
+const { Octokit } = require('@octokit/rest')
+
+const baseOptions = {
+  owner: 'ThoughtWorks-Cleantech',
+  repo: 'cloud-carbon-footprint',
+}
+
+async function main() {
+  const { GITHUB_SHA, GITHUB_TOKEN } = process.env
+  if (!GITHUB_SHA) {
+    throw new Error('GITHUB_SHA is not set')
+  }
+  if (!GITHUB_TOKEN) {
+    throw new Error('GITHUB_TOKEN is not set')
+  }
+
+  const octokit = new Octokit({ auth: GITHUB_TOKEN })
+
+  const date = new Date()
+  const yyyy = date.getUTCFullYear()
+  const mm = String(date.getUTCMonth() + 1).padStart(2, '0')
+  const dd = String(date.getUTCDate()).padStart(2, '0')
+  const baseTagName = `release-${yyyy}-${mm}-${dd}`
+
+  console.log('Requesting existing tags')
+
+  const existingTags = await octokit.repos.listTags({
+    ...baseOptions,
+    per_page: 100,
+  })
+  const existingTagNames = existingTags.data.map((obj) => obj.name)
+
+  let tagName = baseTagName
+  let index = 0
+  while (existingTagNames.includes(tagName)) {
+    index += 1
+    tagName = `${baseTagName}.${index}`
+  }
+
+  console.log(`Creating release tag ${tagName}`)
+
+  const annotatedTag = await octokit.git.createTag({
+    ...baseOptions,
+    tag: tagName,
+    message: tagName,
+    object: GITHUB_SHA,
+    type: 'commit',
+  })
+
+  await octokit.git.createRef({
+    ...baseOptions,
+    ref: `refs/tags/${tagName}`,
+    sha: annotatedTag.data.sha,
+  })
+
+  console.log(`::set-output name=tag_name::${tagName}`)
+}
+
+main().catch((error) => {
+  console.error(error.stack)
+  process.exit(1)
+})
