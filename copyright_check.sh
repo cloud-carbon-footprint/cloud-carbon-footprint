33f509359d8cb72a0b8286521d0800d8d53f446a
Cameron Casher <cameron.casher@thoughtworks.com>
2021-03-03 18:30:16 -0700

    [1068]: add release job for changesets

--- copyright_check.sh
+++ copyright_check.sh
@@ -9,6 +9,7 @@ errorCode=0
 git diff --cached --name-status | while read x file; do
   if [ "$x" == 'D' ]; then continue; fi
   if [ ${x::1} == 'R' ]; then continue; fi
+  if [[ $file == *"CHANGELOG"* ]]; then continue; fi
   holder=$(echo $file | grep -E "^.*\.(ts|tsx|js|jsx|md|sh)$" | wc -l)
   if [ $holder -eq 0 ]; then continue; fi
   if grep "$copyright" $file; then continue; fi
--- packages/api/CHANGELOG.md
+++ packages/api/CHANGELOG.md
@@ -1,5 +1,13 @@
 # cloud-carbon-footprint
 
+## 1.0.1
+
+### Patch Changes
+
+- initial api changeset
+  testing changeset implementation
+  bump api to updated patch version
+
 ## 1.0.0
 
 ### Major Changes
