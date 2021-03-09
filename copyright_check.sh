4b34973b14c218e8bf5bb90e81cb31deb62c985d
Cameron Casher <cameron.casher@thoughtworks.com>
2021-03-08 18:17:08 -0700

    [1068] update copyright sh

--- copyright_check.sh
+++ copyright_check.sh
@@ -7,10 +7,10 @@
 copyright="© 2020 ThoughtWorks, Inc. All rights reserved."
 errorCode=0
 git diff --cached --name-status | while read x file; do
-  if [ "$x" == 'D' ]; then continue; fi
-  if [ ${x::1} == 'R' ]; then continue; fi
-  if [[ $file == *"CHANGELOG"* ]]; then continue; fi
-  if [[ $file == *".changeset"* ]]; then continue; fi
+  if [ "$x" = 'D' ]; then continue; fi
+  if [ ${x::1} = 'R' ]; then continue; fi
+  if [[ $file = *"CHANGELOG"* ]]; then continue; fi
+  if [[ $file = *".changeset"* ]]; then continue; fi
   holder=$(echo $file | grep -E "^.*\.(ts|tsx|js|jsx|md|sh)$" | wc -l)
   if [ $holder -eq 0 ]; then continue; fi
   if grep "$copyright" $file; then continue; fi
