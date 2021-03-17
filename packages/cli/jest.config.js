c9e224eada1047fe51829a884986da136a6e52e9
Dylan Jeffers <dylan.jeffers@thoughtworks.com>
2021-03-17 14:13:16 -0700

    test(cli): fix coverage ignore pattern

--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -18,5 +18,5 @@ module.exports = {
   },
   coverageReporters: ['json', 'lcov', 'text', 'clover'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coveragePathIgnorePatterns: ['<rootDir>/CliPrompts.ts'],
+  coveragePathIgnorePatterns: ['<rootDir>/src/CliPrompts.ts'],
 }
