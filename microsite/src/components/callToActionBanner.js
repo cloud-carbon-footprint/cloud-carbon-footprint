6b47c0029f11b3e85c4c0ad8f749ebafab03138a
Roberto Andres Ayala <robertoayala@Robertos-MacBook-Pro.local>
2021-03-17 17:16:32 +0100

    Call to action banner added

---
+++ microsite/src/components/callToActionBanner.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Link from '@docusaurus/Link'
+import Button from '@material-ui/core/Button'
+import useStyles from './materialStyles'
+
+import styles from './callToActionBanner.module.css'
+
+function CallToActionBanner() {
+  const classes = useStyles()
+  return (
+    <div className={styles.callToActionBannerWrapper}>
+      <div className={styles.text}>
+        <h2>Cloud Carbon Emissions Measurement and Analysis Tool</h2>
+        <p>
+          Understand how your cloud usage impacts our environment and what you
+          can do about it
+        </p>
+      </div>
+      <Link to="docs/getting-started">
+        <Button
+          variant="outlined"
+          classes={{ root: classes.paddingLow }}
+          style={{
+            borderColor: '#ffffff',
+          }}
+        >
+          TRY NOW
+        </Button>
+      </Link>
+    </div>
+  )
+}
+
+export default CallToActionBanner
