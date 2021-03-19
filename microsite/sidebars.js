e4d2da5dc72b9d1f3cf8745b0948f936d7e09a98
Pau Manrique <pau.manrique@thoughtworks.com>
2021-03-19 17:36:22 +0100

    [110] Add AWS doc

--- microsite/sidebars.js
+++ microsite/sidebars.js
@@ -1,5 +1,14 @@
+/*
+ * © 2020 ThoughtWorks, Inc. All rights reserved.
+ */
+
 module.exports = {
   tryNowSidebar: {
     'Get Started': ['getting-started'],
+    'Running Locally': [
+      {
+        'Running the Web App': [{ 'Connect to Real Data': ['aws'] }],
+      },
+    ],
   },
 }
