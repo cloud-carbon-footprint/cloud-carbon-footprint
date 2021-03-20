3fac0f0dfc6841ee9718a008f2d11fadf2adf5d9
Cameron Casher <cameron.casher@thoughtworks.com>
2021-03-19 18:34:39 -0600

    fix: update estimator cache to grab earliest date from data

--- packages/core/src/application/EstimatorCacheFileSystem.ts
+++ packages/core/src/application/EstimatorCacheFileSystem.ts
@@ -13,8 +13,21 @@ export const cachePath = process.env.CCF_CACHE_PATH || 'estimates.cache.json'
 export default class EstimatorCacheFileSystem implements EstimatorCache {
   async getEstimates(request: EstimationRequest): Promise<EstimationResult[]> {
     const estimates = await this.loadEstimates()
-    const startDate = moment.utc(request.startDate)
+    const formatDateToTime = (timestamp: string | Date) =>
+      timestamp instanceof Date
+        ? timestamp.getTime()
+        : new Date(timestamp).getTime()
+
     const endDate = moment.utc(request.endDate)
+    const startDate = estimates.length
+      ? moment.utc(
+          estimates.sort(
+            (a, b) =>
+              formatDateToTime(new Date(a.timestamp)) -
+              formatDateToTime(new Date(b.timestamp)),
+          )[0].timestamp,
+        )
+      : moment.utc(request.startDate)
 
     return estimates.filter(({ timestamp }) => {
       return moment
