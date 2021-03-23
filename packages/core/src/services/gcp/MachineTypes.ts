7ce8b5563bf3acc16be5d5329d424a0c91349e35
Cameron Casher <cameron.casher@thoughtworks.com>
2021-03-23 13:44:56 -0600

    [1050] updates shared cores to enum

--- packages/core/src/services/gcp/MachineTypes.ts
+++ packages/core/src/services/gcp/MachineTypes.ts
@@ -10,6 +10,22 @@ import {
   skyLakeBroadwellHaswellSandyBridgeIvyBridge,
 } from '../../domain/ComputeProcessorTypes'
 
+export enum SHARED_CORE_PROCESSORS {
+  E2_MICRO = 'e2-micro',
+  E2_SMALL = 'e2-small',
+  E2_MEDIUM = 'e2-medium',
+  F1_MICRO = 'f1-micro',
+  G1_SMALL = 'g1-small'
+}
+
+// export const sharedCoreProcessors = [
+//   'e2-micro',
+//   'e2-small',
+//   'e2-medium',
+//   'f1-micro',
+//   'g1-small',
+// ]
+
 export const INSTANCE_TYPE_COMPUTE_PROCESSOR_MAPPING: {
   [instanceType: string]: string[]
 } = {
@@ -21,9 +37,9 @@ export const INSTANCE_TYPE_COMPUTE_PROCESSOR_MAPPING: {
   m2: broadwelCascadeLake,
   m1: broadwellSkylake,
   a2: [COMPUTE_PROCESSOR_TYPES.CASCADE_LAKE],
-  'e2-micro': [COMPUTE_PROCESSOR_TYPES.UNKNOWN],
-  'e2-small': [COMPUTE_PROCESSOR_TYPES.UNKNOWN],
-  'e2-medium': [COMPUTE_PROCESSOR_TYPES.UNKNOWN],
-  'f1-micro': [COMPUTE_PROCESSOR_TYPES.UNKNOWN],
-  'g1-small': [COMPUTE_PROCESSOR_TYPES.UNKNOWN],
+  [SHARED_CORE_PROCESSORS.E2_MICRO]: [COMPUTE_PROCESSOR_TYPES.UNKNOWN],
+  [SHARED_CORE_PROCESSORS.E2_SMALL]: [COMPUTE_PROCESSOR_TYPES.UNKNOWN],
+  [SHARED_CORE_PROCESSORS.E2_MEDIUM]: [COMPUTE_PROCESSOR_TYPES.UNKNOWN],
+  [SHARED_CORE_PROCESSORS.F1_MICRO]:[COMPUTE_PROCESSOR_TYPES.UNKNOWN],
+  [SHARED_CORE_PROCESSORS.G1_SMALL]: [COMPUTE_PROCESSOR_TYPES.UNKNOWN],
 }
