2181421416bf1e7f2c01f4918b4d71def835cef5
Cameron Casher <cameron.casher@thoughtworks.com>
2021-04-05 13:26:04 -0600

    [1150] updates copyright check

--- copyright_check.sh
+++ copyright_check.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-#
-# © 2021 Thoughtworks, Inc. All rights reserved.
-#
-
 copyright="© 2021 Thoughtworks, Inc. All rights reserved."
 errorCode=0
 domain=`git config user.email`
-if [ $domain == *"thoughtworks.com"* ]; then continue; fi
-git diff --cached --name-status | while read x file; do
-  if [ "$x" = 'D' ]; then continue; fi
-  if [ ${x::1} = 'R' ]; then continue; fi
-  if [[ $file = *"CHANGELOG"* ]]; then continue; fi
-  if [[ $file = *".changeset"* ]]; then continue; fi
-  holder=$(echo $file | grep -E "^.*\.(ts|tsx|js|jsx|md|sh)$" | wc -l)
-  if [ $holder -eq 0 ]; then continue; fi
-  if grep "$copyright" $file; then continue; fi
 
-  echo "ERROR: \"$copyright\" not in file: ${file}"
-  exit 1
-done || exit 1
+if [[ "$domain" != *"thoughtworks.com"* ]]; then echo "Thank you for contributing!";
+else
+  git diff --cached --name-status | while read x file; do
+    if [ "$x" = 'D' ]; then continue; fi
+    if [ ${x::1} = 'R' ]; then continue; fi
+    if [[ $file = *"CHANGELOG"* ]]; then continue; fi
+    if [[ $file = *".changeset"* ]]; then continue; fi
+    holder=$(echo $file | grep -E "^.*\.(ts|tsx|js|jsx|md|sh)$" | wc -l)
+    if [ $holder -eq 0 ]; then continue; fi
+    if grep "$copyright" $file; then continue; fi
+
+    echo "ERROR: \"$copyright\" not in file: ${file}"
+    exit 1
+  done || exit 1
+fi
