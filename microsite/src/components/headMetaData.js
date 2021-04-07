ba95d23ade25ce2a3fd8b1520982afb3b7515988
Cameron Casher <cameron.casher@thoughtworks.com>
2021-04-07 16:54:07 -0600

    microsite: refactor meta data

--- microsite/src/components/headMetaData.js
+++ microsite/src/components/headMetaData.js
@@ -2,35 +2,32 @@ import React from 'react'
 import Head from '@docusaurus/Head'
 
 function MetaData() {
+  const title =
+    'Cloud Carbon Footprint - An open source tool to measure and analyze cloud carbon emissions'
+  const description =
+    'An open source tool to measure and analyze cloud carbon emissions'
   return (
     <Head>
-      <title>Cloud Carbon Footprint</title>
-      <meta
-        property="og:title"
-        content="Cloud Carbon Footprint - An open source tool to measure and analyze cloud carbon emissions"
-      ></meta>
+      <title>{title}</title>
+      <description>{description}</description>
+      <img
+        src="https://www.cloudcarbonfootprint.org/img/social-share-image.png"
+        alt="Cloud carbon footprint tool screen capture"
+      />
+      <meta property="og:title" content={title}></meta>
       <meta property="og:type" content="website"></meta>
       <meta
         property="og:url"
         content="https://www.cloudcarbonfootprint.org/"
       ></meta>
-      <meta
-        property="og:description"
-        content="An open source tool to measure and analyze cloud carbon emissions"
-      ></meta>
+      <meta property="og:description" content={description}></meta>
       <meta
         property="og:image"
         content="https://www.cloudcarbonfootprint.org/img/social-share-image.png"
       />
       <meta name="twitter:card" content="summary" />
-      <meta
-        name="twitter:title"
-        content="Cloud Carbon Footprint - An open source tool to measure and analyze cloud carbon emissions"
-      />
-      <meta
-        name="twitter:description"
-        content="An open source tool to measure and analyze cloud carbon emissions"
-      />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
       <meta
         name="twitter:image"
         content="https://www.cloudcarbonfootprint.org/img/twtr-share-image.png"
