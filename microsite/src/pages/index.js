08ae2c697a2d5cf90b77e8b41a012410acee5001
Cameron Casher <cameron.casher@thoughtworks.com>
2021-04-07 14:24:49 -0600

    microsite: update meta data

--- microsite/src/pages/index.js
+++ microsite/src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from '@docusaurus/Head'
 import MainLayout from '../components/mainLayout'
 import {
   Hero,
@@ -11,6 +12,28 @@ import {
 function Home() {
   return (
     <MainLayout>
+      <Head>
+        <title>Cloud Carbon Footprint</title>
+        <meta property="og:title" content="Cloud Carbon Footprint"></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta
+          property="og:url"
+          content="https://www.cloudcarbonfootprint.org/"
+        ></meta>
+        <meta
+          property="og:description"
+          content="An open source tool to measure and analyze cloud carbon emissions"
+        ></meta>
+        <meta
+          property="og:image"
+          content="https://cloud-carbon-footprint.org/img/share-image.png"
+        />
+        <meta property="twitter:card" content="summary" />
+        <meta
+          property="twitter:image"
+          content="https://cloud-carbon-footprint.org/img/share-image-twitter.png"
+        />
+      </Head>
       <Hero />
       <Overview />
       <ValueProp />
