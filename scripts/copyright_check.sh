3e6d6aff7f428d8e183609de358ecd0fae5e5cbc
Cameron Casher <cameron.casher@thoughtworks.com>
2021-04-07 09:46:38 -0600

    fix: updates copyright check to skip microsite

--- scripts/copyright_check.sh
+++ scripts/copyright_check.sh
@@ -11,6 +11,7 @@ else
     if [ ${x::1} = 'R' ]; then continue; fi
     if [[ $file = *"CHANGELOG"* ]]; then continue; fi
     if [[ $file = *".changeset"* ]]; then continue; fi
+    if [[ $file = *"microsite/"* ]]; then continue; fi
     holder=$(echo $file | grep -E "^.*\.(ts|tsx|js|jsx|md|sh)$" | wc -l)
     if [ $holder -eq 0 ]; then continue; fi
     if grep "$copyright" $file; then continue; fi
