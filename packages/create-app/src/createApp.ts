59e459b5032b578ed273c54341041ed46b4e4b6e
Cameron Casher <cameron.casher@thoughtworks.com>
2021-04-09 14:53:40 -0600

    [165] updates create app flow

--- packages/create-app/src/createApp.ts
+++ packages/create-app/src/createApp.ts
@@ -1,93 +1,141 @@
 /*
- * © 2020 ThoughtWorks, Inc. All rights reserved.
+ * © 2021 ThoughtWorks, Inc.
  */
-import os from 'os'
+
 import fs from 'fs-extra'
 import { promisify } from 'util'
+import chalk from 'chalk'
+import inquirer, { Answers, Question } from 'inquirer'
 import { exec as execCb } from 'child_process'
-import { resolve } from 'path'
-import { input, prompt } from 'typed-prompts'
+import { resolve as resolvePath } from 'path'
+import os from 'os'
 
-import { templatingTask } from './lib/templating'
+import { Task, templatingTask } from './lib/tasks'
 import { getTargetDir } from './lib/paths'
 
 const exec = promisify(execCb)
 
 async function checkExists(rootDir: string, name: string) {
-  const destination = resolve(rootDir, name)
-  if (await fs.pathExists(destination)) {
-    const existing = destination.replace(`${rootDir}/`, '')
-    throw new Error(
-      `A directory with the same name already exists: ${existing}\nPlease try again with a different app name`,
-    )
-  }
+  await Task.forItem('checking', name, async () => {
+    const destination = resolvePath(rootDir, name)
+
+    if (await fs.pathExists(destination)) {
+      const existing = chalk.cyan(destination.replace(`${rootDir}/`, ''))
+      throw new Error(
+        `A directory with the same name already exists: ${existing}\nPlease try again with a different app name`,
+      )
+    }
+  })
 }
 
 async function createTemporaryAppFolder(tempDir: string) {
-  try {
-    await fs.mkdir(tempDir)
-  } catch (error) {
-    throw new Error(
-      `Failed to create temporary app directory: ${error.message}`,
-    )
-  }
+  await Task.forItem('creating', 'temporary directory', async () => {
+    try {
+      await fs.mkdir(tempDir)
+    } catch (error) {
+      throw new Error(
+        `Failed to create temporary app directory: ${error.message}`,
+      )
+    }
+  })
 }
 
-async function moveApp(tempDir: string, destination: string, id: string) {
-  try {
-    await fs.move(tempDir, destination)
-  } catch (error) {
-    throw new Error(
-      `Failed to move app from ${tempDir} to ${destination}: ${error.message}`,
-    )
-  }
+async function cleanUp(tempDir: string) {
+  await Task.forItem('remove', 'temporary directory', async () => {
+    await fs.remove(tempDir)
+  })
 }
 
 async function buildApp(appDir: string) {
   const runCmd = async (cmd: string) => {
-    process.chdir(appDir)
-
-    try {
-      await exec(cmd)
-    } catch (error) {
-      process.stdout.write(error.stderr)
-      process.stdout.write(error.stdout)
-      throw new Error(`Could not execute command ${cmd}`)
-    }
+    await Task.forItem('executing', cmd, async () => {
+      process.chdir(appDir)
+      try {
+        await exec(cmd)
+      } catch (error) {
+        process.stdout.write(error.stderr)
+        process.stdout.write(error.stdout)
+        throw new Error(`Could not execute command ${chalk.cyan(cmd)}`)
+      }
+    })
   }
   await runCmd('yarn install')
 }
 
+async function moveApp(tempDir: string, destination: string, id: string) {
+  await Task.forItem('moving', id, async () => {
+    try {
+      await fs.move(tempDir, destination)
+    } catch (error) {
+      throw new Error(
+        `Failed to move app from ${tempDir} to ${destination}: ${error.message}`,
+      )
+    }
+  })
+}
+
 export default async (): Promise<void> => {
-  const answers = await prompt([
-    input('name', 'Enter a name for the app [required]'),
-  ])
+  const questions: Question[] = [
+    {
+      type: 'input',
+      name: 'name',
+      message: chalk.blue('Enter a name for the app [required]'),
+      validate: (value: any) => {
+        if (!value) {
+          return chalk.red('Please enter a name for the app')
+        } else if (!/^[a-z0-9]+(-[a-z0-9]+)*$/.test(value)) {
+          return chalk.red(
+            'App name must be kebab-cased and contain only letters, digits, and dashes.',
+          )
+        }
+        return true
+      },
+    },
+  ]
+
+  const answers: Answers = await inquirer.prompt(questions)
 
   const targetDirectory = getTargetDir()
-  const templateDir = resolve(__dirname, '../templates/default-app')
-  const tempDir = resolve(os.tmpdir(), answers.name)
-  const appDir = resolve(targetDirectory, answers.name)
+  const templateDir = resolvePath(__dirname, '../templates/default-app')
+  const tempDir = resolvePath(os.tmpdir(), answers.name)
+  const appDir = resolvePath(targetDirectory, answers.name)
+
+  Task.log()
+  Task.log('Creating the app...')
 
   try {
-    console.log('Checking if the directory is available')
+    Task.section('Checking if the directory is available')
     await checkExists(targetDirectory, answers.name)
 
-    console.log('Creating a temporary app directory')
+    Task.section('Creating a temporary app directory')
     await createTemporaryAppFolder(tempDir)
 
-    console.log('Preparing files')
+    Task.section('Preparing files')
     await templatingTask(templateDir, tempDir, answers)
 
-    console.log('Moving to final location')
+    Task.section('Moving to final location')
     await moveApp(tempDir, appDir, answers.name)
 
-    console.log('Building the app')
+    Task.section('Building the app')
     await buildApp(appDir)
 
-    console.log(
-      `Successfully created Cloud Carbon Footprint app: ${answers.name}`,
+    Task.log()
+    Task.log(
+      chalk.green(
+        `🥇  Successfully created Cloud Carbon Footprint app: ${chalk.cyan(
+          answers.name,
+        )}`,
+      ),
     )
+    Task.log()
+    Task.exit()
   } catch (error) {
-    console.log(`There was an error creating your App: ${error.message}`)
+    Task.log(`There was an error creating your App: ${error.message}`)
+    Task.log('We are going to clean up, and then you can try again.')
+
+    Task.section('Cleanup')
+    await cleanUp(tempDir)
+    Task.error('Failed to create app!')
+    Task.exit(1)
   }
 }
---
+++ packages/create-app/templates/default-app/packages/cli/src/GuidedInstall/runner.ts
@@ -0,0 +1,52 @@
+/*
+ * © 2021 ThoughtWorks, Inc.
+ */
+
+import { createEnvFile, EnvConfig, listPrompt, microsite } from './common'
+import { log } from './external'
+import { AWSSetup } from './aws'
+import { GCPSetup } from './gcp'
+import { AzureSetup } from './azure'
+
+type ProvidersConfig = {
+  [key: string]: { setup: () => Promise<EnvConfig>; docs: string }
+}
+const providers: ProvidersConfig = {
+  AWS: {
+    setup: AWSSetup,
+    docs: '/docs/aws',
+  },
+  GCP: {
+    setup: GCPSetup,
+    docs: '/docs/gcp',
+  },
+  Azure: {
+    setup: AzureSetup,
+    docs: '/docs/azure',
+  },
+}
+
+export async function GuidedInstall(): Promise<void> {
+  log(
+    `Please follow the following prompts to configure Cloud Carbon Footprint to connect to a cloud provider. To connect to multiple cloud providers, please repeat this script for each additional provider. For an understanding of the default configurations and other configuration options, please visit the docs on the microsite: ${microsite}/docs/introduction`,
+  )
+
+  const provider: string = await listPrompt(
+    'Choose a cloud provider to configure:',
+    Object.keys(providers),
+  )
+
+  const { setup, docs } = providers[provider]
+  log(`${provider} setup. See ${microsite}${docs} for additional details.`)
+  const env = await setup()
+
+  await createEnvFile('./', env)
+  await createEnvFile('../api/', env)
+
+  log(
+    `Your Cloud Carbon Footprint application is now configured to use your cloud provider data. You can see these settings in the .env files listed above. To connect to an additional cloud provider or update an existing one, please re-run this script. For questions or more configuration options, please visit ${microsite}/docs/introduction`,
+  )
+  log(
+    `Please make sure your appConfig object in the client Config file (packages/client/src/Config.ts) includes your cloud provider(s) in the CURRENT_PROVIDERS array`,
+  )
+}
