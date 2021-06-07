64ae33b095a89f5949834d46291c1d395ee23c96
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2021-06-07 15:38:21 +1000

    [319] Adds initial tests for Cost.ts | Dan

---
+++ packages/core/src/cost/__tests__/Cost.test.ts
@@ -0,0 +1,54 @@
+/*
+ * © 2021 ThoughtWorks, Inc.
+ */
+
+import Cost, { aggregateCostsByDay } from '../Cost'
+
+describe('Cost', () => {
+  const dayOne = new Date('2021-01-01')
+  const dayTwo = new Date('2021-01-02')
+
+  it('aggregateCostsByDay', () => {
+    // given
+    const testCosts: Cost[] = [
+      {
+        timestamp: dayOne,
+        amount: 10,
+        currency: 'USD',
+      },
+      {
+        timestamp: dayOne,
+        amount: 40,
+        currency: 'USD',
+      },
+      {
+        timestamp: dayTwo,
+        amount: 100,
+        currency: 'USD',
+      },
+      {
+        timestamp: dayTwo,
+        amount: 200,
+        currency: 'USD',
+      },
+    ]
+
+    // when
+    const result = aggregateCostsByDay(testCosts)
+
+    // then
+    const expectedResult = {
+      '2021-01-01': {
+        amount: 50,
+        currency: 'USD',
+        timestamp: dayOne,
+      },
+      '2021-01-02': {
+        amount: 300,
+        currency: 'USD',
+        timestamp: dayTwo,
+      },
+    }
+    expect(result).toEqual(expectedResult)
+  })
+})
