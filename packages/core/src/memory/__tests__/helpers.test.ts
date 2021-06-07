57c9154869c45e12db8c990887e66a36d38a11c7
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2021-06-07 15:49:17 +1000

    [319] Adds initial tests for helpers.ts | Dan

---
+++ packages/core/src/memory/__tests__/helpers.test.ts
@@ -0,0 +1,29 @@
+/*
+ * © 2021 ThoughtWorks, Inc.
+ */
+
+import { calculateGigabyteHours, getPhysicalChips } from '../helpers'
+
+describe('helpers', () => {
+  it('getPhysicalChips', () => {
+    // when
+    const result = getPhysicalChips(300)
+
+    // then
+    expect(result).toEqual(4)
+  })
+
+  it('calculateGigabyteHours gigabyteHours is undefined', () => {
+    // when
+    const result = calculateGigabyteHours(1, 1, 2, 0, 0)
+    // then
+    expect(result).toBeUndefined()
+  })
+
+  it('calculateGigabyteHours gigabyteHours is not undefined', () => {
+    // when
+    const result = calculateGigabyteHours(1, 10, 5, 20, 20)
+    // then
+    expect(result).toEqual(200)
+  })
+})
