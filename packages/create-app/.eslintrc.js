acd29286ea6cc930f156a7242227581aa3344f05
Cameron Casher <cameron.casher@thoughtworks.com>
2021-06-09 17:12:31 -0600

    fixes create-app issues

--- packages/create-app/.eslintrc.js
+++ packages/create-app/.eslintrc.js
@@ -10,11 +10,13 @@ module.exports = {
   },
   extends: [
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'prettier', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   plugins: ['unused-imports'],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. '@typescript-eslint/explicit-function-return-type': 'off',
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     'no-unused-vars': 'off',
     'unused-imports/no-unused-imports-ts': 'error',
     '@typescript-eslint/no-unused-vars': [
--- packages/create-app/templates/default-app/packages/cli/.eslintrc.js
+++ packages/create-app/templates/default-app/packages/cli/.eslintrc.js
@@ -10,8 +10,6 @@ module.exports = {
   },
   extends: [
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   plugins: ['unused-imports'],
   rules: {
