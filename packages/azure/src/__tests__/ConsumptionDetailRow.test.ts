18c50f89c77cc7922f23fb7a71774819a8a60df8
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2021-09-08 18:09:32 -0400

    [466] Fixes ConsumptionDetailRow test to fix the build | Dan

--- packages/azure/src/__tests__/ConsumptionDetailRow.test.ts
+++ packages/azure/src/__tests__/ConsumptionDetailRow.test.ts
@@ -2,16 +2,24 @@
  * © 2021 Thoughtworks, Inc.
  */
 
+import { LegacyUsageDetail } from '@azure/arm-consumption/esm/models'
+
 import ConsumptionDetailRow from '../lib/ConsumptionDetailRow'
-import { mockConsumptionManagementResponseOne } from './fixtures/consumptionManagement.fixtures'
 
 describe('ConsumptionDetailRow', () => {
-  it('should return 1 vcpu as default when intance family is unkwon', () => {
-    const computeComsumtionDetails = mockConsumptionManagementResponseOne[0]
-    computeComsumtionDetails.meterDetails.meterName = 'test-meter-name'
+  it('should return 1 vCpu as default when instance family is unknown', () => {
+    //given
+    const computeConsumptionDetails: LegacyUsageDetail = {
+      kind: null,
+      meterDetails: {
+        meterName: 'test-usageType',
+      },
+    }
 
     //when
-    const result = new ConsumptionDetailRow(computeComsumtionDetails).getVCpus()
+    const result = new ConsumptionDetailRow(
+      computeConsumptionDetails,
+    ).getVCpus()
 
     //then
     expect(result).toEqual(1)
