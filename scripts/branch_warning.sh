420f7d30ca104f7e5e329bfad07c28347b6e37ac
Johannes Grumboeck <johannes.grumboeck@redbull.com>
2021-10-27 10:51:19 +0200

    Detect non-interactive pre-commit hook calls by IDE

--- scripts/branch_warning.sh
+++ scripts/branch_warning.sh
@@ -7,7 +7,14 @@
 branch="$(git rev-parse --abbrev-ref HEAD)"
 
 if [[ "$branch" != "trunk" ]]; then
-    read -p "Are you sure you want to use a branch that isn't trunk? (y/n): " -n 1 -r < /dev/tty
+    if [ -t 1 ]; then
+        # We're running interactive
+        read -p "Are you sure you want to use a branch that isn't trunk? (y/n): " -n 1 -r < /dev/tty
+    else
+        # We're running non-interactive (possibly called by IDE)
+        echo "This is not the trunk branch and we assume that's intended."
+        REPLY="Y"
+    fi
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         echo "Okie dokie. Be Careful out there :)"
