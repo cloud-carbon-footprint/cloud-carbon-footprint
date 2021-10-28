eb380f8942d5c610114929ea09bed02faf9b3b37
Jeff <jeff.zinger@thoughtworks.com>
2021-10-28 14:59:34 +0200

    [495] fixed search input for co2 savings if units are in kg

--- packages/client/src/pages/RecommendationsPage/RecommendationsTable/RecommendationsTable.tsx
+++ packages/client/src/pages/RecommendationsPage/RecommendationsTable/RecommendationsTable.tsx
@@ -165,7 +165,9 @@ const RecommendationsTable: FunctionComponent<RecommendationsTableProps> = ({
           if (!fieldsToNotFilter.includes(field)) {
             let value = row[field]
             if (field.includes('Savings')) {
-              value = Math.round(value * 1000) / 1000
+              value = useKilograms
+                ? Math.round(value * 1000)
+                : Math.round(value * 1000) / 1000
             }
             return searchRegex.test(value?.toString())
           }
