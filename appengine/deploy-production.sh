b65def0f970fe9db08952471d446defac910cbd9
Tobias Gerbothe <tobias.gerbothe@ottogroup.com>
2022-04-14 11:49:35 +0200

    Remove deployment debug flags

--- appengine/deploy-production.sh
+++ appengine/deploy-production.sh
@@ -8,11 +8,9 @@ set -x
 
 gcloud --quiet app deploy \
   --version=production \
-  --log-http --verbosity debug \
   ../dist-workspace/packages/api/app-production.yaml
 gcloud --quiet app deploy \
   --version=production \
-  --log-http --verbosity debug \
   ../dist-workspace/packages/client/app-production.yaml
 gcloud --quiet app deploy \
   dispatch.yaml
