e2013e60f1f92c2d3ef3c100aba30fd72b0e68d7
Charles Korn <ckorn@thoughtworks.com>
2022-09-01 15:23:29 +1000

    Refactor configuration tests to ensure environment variables are restored to their original state.

--- packages/common/src/__tests__/Config.test.ts
+++ packages/common/src/__tests__/Config.test.ts
@@ -6,46 +6,71 @@ import fs from 'fs'
 import getConfig from '../Config'
 
 describe('Config', () => {
+  const withEnvironment = (name: string, value: string, test: () => void) => {
+    const existingValue = process.env[name]
+    process.env[name] = value
+
+    try {
+      test()
+    } finally {
+      if (existingValue === undefined) {
+        delete process.env[name]
+      } else {
+        process.env[name] = existingValue
+      }
+    }
+  }
+
   it('get AWS accounts', () => {
     const id = 'b47m4n'
     const name = 'Bruce Wayne'
-    process.env.AWS_ACCOUNTS = `[{"id": "${id}", "name": "${name}"}]`
-    const config = getConfig()
-    expect(config.AWS.accounts[0].id).toBe(id)
-    expect(config.AWS.accounts[0].name).toBe(name)
-    // Avoid side effects
-    delete process.env.AWS_ACCOUNTS
+
+    withEnvironment(
+      'AWS_ACCOUNTS',
+      `[{"id": "${id}", "name": "${name}"}]`,
+      () => {
+        const config = getConfig()
+        expect(config.AWS.accounts[0].id).toBe(id)
+        expect(config.AWS.accounts[0].name).toBe(name)
+      },
+    )
   })
 
   it('loads AWS resource tags from environment variables', () => {
-    process.env.AWS_RESOURCE_TAG_NAMES = `["Environment"]`
-    const config = getConfig()
-    expect(config.AWS.RESOURCE_TAG_NAMES).toEqual(['Environment'])
-    delete process.env.AWS_RESOURCE_TAG_NAMES // FIXME: refactor this to reset back to original value
+    withEnvironment('AWS_RESOURCE_TAG_NAMES', `["Environment"]`, () => {
+      const config = getConfig()
+      expect(config.AWS.RESOURCE_TAG_NAMES).toEqual(['Environment'])
+    })
   })
 
   it('get GCP projects', () => {
     const id = 'id'
     const name = 'project'
-    process.env.GCP_PROJECTS = `[{"id": "${id}", "name": "${name}"}]`
-    const config = getConfig()
-    expect(config.GCP.projects[0].id).toBe(id)
-    expect(config.GCP.projects[0].name).toBe(name)
-    // Avoid side effects
-    delete process.env.GCP_PROJECTS
+
+    withEnvironment(
+      'GCP_PROJECTS',
+      `[{"id": "${id}", "name": "${name}"}]`,
+      () => {
+        const config = getConfig()
+        expect(config.GCP.projects[0].id).toBe(id)
+        expect(config.GCP.projects[0].name).toBe(name)
+      },
+    )
   })
 
   it('get environment variable from `process.env`', () => {
     const fsSpy = jest.spyOn(fs, 'readFileSync').mockImplementation(() => {
       throw new Error()
     })
+
     const billingId = 'ezb1'
-    process.env.AWS_BILLING_ACCOUNT_ID = billingId
-    const config = getConfig()
-    expect(fsSpy).toBeCalled()
-    expect(config.AWS.BILLING_ACCOUNT_ID).toBe(billingId)
-    // Avoid side effects
-    delete process.env.AWS_BILLING_ACCOUNT_ID
+
+    withEnvironment('AWS_BILLING_ACCOUNT_ID', billingId, () => {
+      const config = getConfig()
+      expect(fsSpy).toBeCalled()
+      expect(config.AWS.BILLING_ACCOUNT_ID).toBe(billingId)
+    })
+
     jest.clearAllMocks()
   })
 })
