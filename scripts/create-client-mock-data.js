afb07fcda2f5c87232c565e044fecf911b349740
Ashutosh Krishna <ashutosh.krishna@thoughtworks.com>
2022-10-04 11:40:14 +0530

    Add logic to update timestamp in mockData.json

--- scripts/create-client-mock-data.js
+++ scripts/create-client-mock-data.js
@@ -5,6 +5,10 @@
 const fs = require('fs')
 const path = require('path')
 
+const prevMonth = new Date().getMonth() - 1
+let updatedMonth = new Date(2022, prevMonth, 26)
+updatedMonth.setUTCHours(0, 0, 0, 0)
+
 async function main() {
   const data = fs.readFileSync(
     path.resolve(__dirname, `../packages/client/stub-server/mockData.json`),
@@ -12,6 +16,24 @@ async function main() {
   )
 
   const mockData = JSON.parse(data)
+
+  mockData.footprint.forEach((footprint) => {
+    let updatedMonthStr = updatedMonth.toISOString()
+    footprint.timestamp = updatedMonthStr
+    updatedMonth = getPreviousMonth(updatedMonth)
+    updatedMonth.setUTCHours(0, 0, 0, 0)
+  })
+
+  fs.writeFile(
+    path.resolve(__dirname, `../packages/client/stub-server/mockData.json`),
+    JSON.stringify(mockData),
+    (err) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+    },
+  )
 
   Object.keys(mockData).forEach((key) => {
     const pathSuffix = key === 'emissions' ? `regions/${key}` : key
@@ -30,6 +52,11 @@ async function main() {
       },
     )
   })
+}
+
+function getPreviousMonth(timestamp) {
+  const prevMonth = timestamp.getMonth() - 1
+  return new Date(timestamp.getFullYear(), prevMonth, 26)
 }
 
 main().catch((error) => {
