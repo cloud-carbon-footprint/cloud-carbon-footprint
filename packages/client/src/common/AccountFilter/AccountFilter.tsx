f435df758e21ae9e41182ca787b68706f41bed54
Yanamandra Pranati <pranati.yanamandra@Yanamandras-MacBook-Pro.local>
2022-10-21 10:54:48 +0530

    <PY|TS|SU> |  Added time forecast recommendations for scheduling workload operations

--- packages/client/src/common/AccountFilter/AccountFilter.tsx
+++ packages/client/src/common/AccountFilter/AccountFilter.tsx
@@ -1,6 +1,8 @@
-/*
- * © 2021 Thoughtworks, Inc.
- */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-function */
+// /*
+//  * © 2021 Thoughtworks, Inc.
+//  */
 
 import React, { FunctionComponent } from 'react'
 import FilterDropdown from '../../common/FilterDropdown'
@@ -36,3 +38,36 @@ const AccountFilter: FunctionComponent<FilterProps> = ({
 }
 
 export default AccountFilter
+
+/*
+ * © 2021 Thoughtworks, Inc.
+ */
+
+// import React, { FunctionComponent, useState } from 'react'
+// import { DropdownFilterOptions, FilterProps } from '../../Types'
+// import SelectDropdown from '../../common/SelectDropdown'
+
+// const AccountFilter: FunctionComponent<FilterProps> = ({
+//   filters,
+//   setFilters,
+//   options,
+// }) => {
+//   const [accountType, setAccountType] = useState()
+//   const stringAccountOptions: string[] = options.accounts.map((account) => {
+//     return account.name
+//   })
+
+//   const accountOptions = options.accounts
+//   return (
+//     <SelectDropdown
+//       id="accounts-filter"
+//       value={filters.label(accountOptions, DropdownFilterOptions.ACCOUNTS)}
+//       dropdownOptions={stringAccountOptions}
+//       handleChange={(event) => {
+//         console.log(event)
+//       }}
+//     />
+//   )
+// }
+
+// export default AccountFilter
