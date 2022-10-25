5a65925fb52a78584577671bc374d04f72643797
Tanvi Sutar <tanvi@Tanvis-MacBook-Pro.local>
2022-10-25 16:53:42 +0530

    [TS/PY/SU] Add time forecast tableto recommend optimal time for scheduling workloads

--- packages/client/src/utils/hooks/ForecastDataHook.ts
+++ packages/client/src/utils/hooks/ForecastDataHook.ts
@@ -1,16 +1,44 @@
 import useRemoteForecastService from './ForecastServiceHook'
+import axios from 'axios'
+
 export interface RegionData {
   location: string
   workLoadExecutionTime: number
 }
+
 export interface OptimalTime {
+  region: string
   location: string
   timestamp: string
   duration: number
   value: number
 }
 
-export const useForecastData = (params: RegionData): OptimalTime[] => {
-  const data: OptimalTime[] = useRemoteForecastService(params)
-  return data
+export const useForecastData = (
+  params: Map<string, string[]>,
+): Map<string, OptimalTime[]> => {
+  const baseUrl = `http://localhost:5073/emissions/forecasts/current`
+
+  const promiseArray = []
+  for (const parameter of params) {
+    const promise = axios.get(
+      `${baseUrl}?location=${parameter[1][0]}&windowSize=${parameter[1][1]}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
+      },
+    )
+    promiseArray.push(promise)
+  }
+
+  const optimalDataPointArray = useRemoteForecastService(promiseArray)
+  let index = 0
+  const optimalTimeMap = new Map()
+  for (const [key] of params) {
+    optimalTimeMap.set(key, optimalDataPointArray[index++])
+  }
+
+  return optimalTimeMap
 }
