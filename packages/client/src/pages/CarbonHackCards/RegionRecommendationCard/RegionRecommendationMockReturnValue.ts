688af3d7a2481fc7e8082ba542f7c588b8e8ada4
Sanika Unavane <sanika.unavane@Sanikas-MacBook-Pro.local>
2022-11-09 17:29:09 +0530

    <SU|TS|PY> Added unit tests

---
+++ packages/client/src/pages/CarbonHackCards/RegionRecommendationCard/RegionRecommendationMockReturnValue.ts
@@ -0,0 +1,160 @@
+import moment from 'moment'
+
+const dateOne = new Date('2020-07-10T00:00:00.000Z')
+
+const regionRecommendationMockReturnValue = [
+  [
+    {
+      carbonIntensity: 617.3369634751824,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-east-1',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 617.3323057336263,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-east-2',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 394.3501456764055,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-west-1',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 592.8319150022206,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-west-2',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 617.3323057336263,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-gov-east-1',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 592.8319150022206,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-gov-west-1',
+      startTime: dateOne,
+    },
+  ],
+  [
+    {
+      carbonIntensity: 617.3369634751824,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-east-1',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 617.3323057336263,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-east-2',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 394.3501456764055,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-west-1',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 592.8319150022206,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-west-2',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 617.3323057336263,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-gov-east-1',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 592.8319150022206,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-gov-west-1',
+      startTime: dateOne,
+    },
+  ],
+  [
+    {
+      carbonIntensity: 617.3369634751824,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-east-1',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 617.3323057336263,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-east-2',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 394.3501456764055,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-west-1',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 592.8319150022206,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-west-2',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 617.3323057336263,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-gov-east-1',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 592.8319150022206,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-gov-west-1',
+      startTime: dateOne,
+    },
+  ],
+  [
+    {
+      carbonIntensity: 617.3369634751824,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-east-1',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 617.3323057336263,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-east-2',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 394.3501456764055,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-west-1',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 592.8319150022206,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-west-2',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 617.3323057336263,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-gov-east-1',
+      startTime: dateOne,
+    },
+    {
+      carbonIntensity: 592.8319150022206,
+      endTime: moment(dateOne).add(1, 'd').toDate(),
+      location: 'us-gov-west-1',
+      startTime: dateOne,
+    },
+  ],
+]
+
+export default regionRecommendationMockReturnValue
