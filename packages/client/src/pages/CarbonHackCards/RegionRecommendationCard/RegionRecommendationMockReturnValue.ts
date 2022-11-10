6742651300219536f77ff2b5d83de9c11d000a74
Yanamandra Pranati <pranati.yanamandra@Yanamandras-MacBook-Pro.local>
2022-11-10 16:53:19 +0530

    <PY|TS|SU> Added tests and added loader for components.

--- packages/client/src/pages/CarbonHackCards/RegionRecommendationCard/RegionRecommendationMockReturnValue.ts
+++ packages/client/src/pages/CarbonHackCards/RegionRecommendationCard/RegionRecommendationMockReturnValue.ts
@@ -1,160 +1,162 @@
-import moment from 'moment'
+const dateOne = '2020-07-10T00:00:00.000Z'
 
-const dateOne = new Date('2020-07-10T00:00:00.000Z')
-
-const regionRecommendationMockReturnValue = [
-  [
-    {
-      carbonIntensity: 617.3369634751824,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-east-1',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 617.3323057336263,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-east-2',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 394.3501456764055,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-west-1',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 592.8319150022206,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-west-2',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 617.3323057336263,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-gov-east-1',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 592.8319150022206,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-gov-west-1',
-      startTime: dateOne,
-    },
+const regionRecommendationMockReturnValue = {
+  data: [
+    [
+      {
+        carbonIntensity: 617.3369634751824,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-east-1',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 617.3323057336263,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-east-2',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 394.3501456764055,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-west-1',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 592.8319150022206,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-west-2',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 617.3323057336263,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-gov-east-1',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 592.8319150022206,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-gov-west-1',
+        startTime: dateOne,
+      },
+    ],
+    [
+      {
+        carbonIntensity: 617.3369634751824,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-east-1',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 617.3323057336263,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-east-2',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 394.3501456764055,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-west-1',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 592.8319150022206,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-west-2',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 617.3323057336263,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-gov-east-1',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 592.8319150022206,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-gov-west-1',
+        startTime: dateOne,
+      },
+    ],
+    [
+      {
+        carbonIntensity: 617.3369634751824,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-east-1',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 617.3323057336263,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-east-2',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 394.3501456764055,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-west-1',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 592.8319150022206,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-west-2',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 617.3323057336263,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-gov-east-1',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 592.8319150022206,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-gov-west-1',
+        startTime: dateOne,
+      },
+    ],
+    [
+      {
+        carbonIntensity: 617.3369634751824,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-east-1',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 617.3323057336263,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-east-2',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 394.3501456764055,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-west-1',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 592.8319150022206,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-west-2',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 617.3323057336263,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-gov-east-1',
+        startTime: dateOne,
+      },
+      {
+        carbonIntensity: 592.8319150022206,
+        endTime: '2020-07-15T00:00:00.000Z',
+        location: 'us-gov-west-1',
+        startTime: dateOne,
+      },
+    ],
   ],
-  [
-    {
-      carbonIntensity: 617.3369634751824,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-east-1',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 617.3323057336263,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-east-2',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 394.3501456764055,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-west-1',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 592.8319150022206,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-west-2',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 617.3323057336263,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-gov-east-1',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 592.8319150022206,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-gov-west-1',
-      startTime: dateOne,
-    },
-  ],
-  [
-    {
-      carbonIntensity: 617.3369634751824,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-east-1',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 617.3323057336263,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-east-2',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 394.3501456764055,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-west-1',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 592.8319150022206,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-west-2',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 617.3323057336263,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-gov-east-1',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 592.8319150022206,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-gov-west-1',
-      startTime: dateOne,
-    },
-  ],
-  [
-    {
-      carbonIntensity: 617.3369634751824,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-east-1',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 617.3323057336263,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-east-2',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 394.3501456764055,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-west-1',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 592.8319150022206,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-west-2',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 617.3323057336263,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-gov-east-1',
-      startTime: dateOne,
-    },
-    {
-      carbonIntensity: 592.8319150022206,
-      endTime: moment(dateOne).add(1, 'd').toDate(),
-      location: 'us-gov-west-1',
-      startTime: dateOne,
-    },
-  ],
-]
+  error: null,
+  loading: false,
+}
 
 export default regionRecommendationMockReturnValue
--- packages/client/src/utils/hooks/RegionRecommendationServiceHook.ts
+++ packages/client/src/utils/hooks/RegionRecommendationServiceHook.ts
@@ -5,21 +5,35 @@
 
 import { useEffect } from 'react'
 import React from 'react'
+import { BestLocationData } from './RegionRecommendationDataHook'
+import { ServiceResult } from 'src/Types'
 
-const useRemoteRegionRecommendationService = (params: any): any => {
-  const [result, setResult] = React.useState<any>()
+const useRemoteRegionRecommendationService = (
+  params: any,
+): ServiceResult<BestLocationData[]> => {
+  const [data, setData] = React.useState<BestLocationData[][]>([])
+  const [loading, setLoading] = React.useState(true)
+  const [error, setError] = React.useState<Error>(null)
   useEffect(() => {
     const fetchBestLocation = async () => {
-      const response = await Promise.all(params)
-      setResult(
-        response.map((res) => {
-          return res.data
-        }),
-      )
+      try {
+        const response = await Promise.all(params)
+        setData(
+          response.map((res) => {
+            return res.data
+          }),
+        )
+        setLoading(false)
+      } catch (e) {
+        setError(e)
+      }
     }
     fetchBestLocation()
+    return () => {
+      setLoading(false)
+    }
   }, [])
-  return result
+  return { data, error, loading }
 }
 
 export default useRemoteRegionRecommendationService
