6c042941353773658ce64cfe9c41e057822589ea
Tanvi Sutar <tanvi@Tanvis-MacBook-Pro.local>
2022-11-16 15:22:07 +0530

    <TS|PY|SU> Added pagination to forecast and region recommendation tables

--- packages/client/src/pages/CarbonHackCards/ForecastCard/ForecastCard.tsx
+++ packages/client/src/pages/CarbonHackCards/ForecastCard/ForecastCard.tsx
@@ -4,17 +4,13 @@ import React, { FunctionComponent, ReactElement } from 'react'
 import { useForecastData } from '../../../utils/hooks/ForecastDataHook'
 import useStyles from '../../EmissionsMetricsPage/CarbonComparisonCard/carbonComparisonStyles'
 import DashboardCard from '../../../layout/DashboardCard'
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
-import Paper from '@mui/material/Paper'
 import { Typography, CardContent } from '@material-ui/core'
 import moment from 'moment'
-import LineChartDialog from './LineChartDialog'
 import LoadingMessage from '../../../common/LoadingMessage'
+import { DataGrid, GridColumns } from '@mui/x-data-grid'
+import LineChartDialog from './LineChartDialog'
+import ErrorPage from '../../../layout/ErrorPage'
+
 const ForecastCard: FunctionComponent<any> = ({ data }): ReactElement => {
   const convertUTCtoLocalTime = (utcTime) => {
     const stillUtc = moment.utc(utcTime).toDate()
@@ -34,60 +30,95 @@ const ForecastCard: FunctionComponent<any> = ({ data }): ReactElement => {
   })
 
   const optimalRegionTimeMap = (result) => {
-    // eslint-disable-next-line prefer-const
-    let array = []
+    const row = []
     for (const [key, value] of result) {
       if (value != undefined) {
         value.optimalDataPoints.map((eachOptimalTime) => {
           const [date, time] = convertUTCtoLocalTime(eachOptimalTime.timestamp)
-          array.push(
-            <TableRow
-              key={time}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell align="center">{key}</TableCell>
-              <TableCell align="center">
-                {accountRegionMap.get(key)[0]}
-              </TableCell>
-              <TableCell align="center">{date}</TableCell>
-              <TableCell align="center">{time}</TableCell>
-              <TableCell align="center">{eachOptimalTime.value}</TableCell>
-              <TableCell align="center">
-                <LineChartDialog
-                  testId="line-chart-dialog"
-                  forecastData={value.forecastData}
-                  region={accountRegionMap.get(key)[0]}
-                ></LineChartDialog>
-              </TableCell>
-            </TableRow>,
-          )
+          row.push({
+            id: key,
+            region: accountRegionMap.get(key)[0],
+            date: date,
+            time: time,
+            rating: eachOptimalTime.value,
+            moreinfo: {
+              forecastData: value.forecastData,
+              region: accountRegionMap.get(key)[0],
+            },
+          })
         })
       }
     }
-    return array
+    return row
   }
 
   const dataToSend = accountRegionMap
 
   const { result, error, loading } = useForecastData(dataToSend)
 
+  const columns: GridColumns = [
+    {
+      field: 'id',
+      headerName: 'Account',
+      width: 265,
+      headerAlign: 'center',
+      align: 'center',
+    },
+    {
+      field: 'region',
+      headerName: 'Region',
+      width: 265,
+      headerAlign: 'center',
+      align: 'center',
+    },
+    {
+      field: 'date',
+      headerName: 'Date',
+      width: 265,
+      headerAlign: 'center',
+      align: 'center',
+    },
+    {
+      field: 'time',
+      headerName: 'Time',
+      width: 265,
+      headerAlign: 'center',
+      align: 'center',
+    },
+    {
+      field: 'rating',
+      headerName: 'Rating (g/kWh)',
+      width: 265,
+      headerAlign: 'center',
+      align: 'center',
+    },
+    {
+      field: 'moreinfo',
+      headerName: 'More Info',
+      width: 265,
+      headerAlign: 'center',
+      align: 'center',
+      renderCell: (params) => (
+        <LineChartDialog
+          testId="line-chart-dialog"
+          forecastData={params.value.forecastData}
+          region={params.value.region}
+        ></LineChartDialog>
+      ),
+    },
+  ]
+
   const renderTable = () => {
     return (
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="center">Account</TableCell>
-              <TableCell align="center">Region</TableCell>
-              <TableCell align="center">Date</TableCell>
-              <TableCell align="center">Time</TableCell>
-              <TableCell align="center">Rating (g/kWh)</TableCell>
-              <TableCell align="center">More Info</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>{optimalRegionTimeMap(result)}</TableBody>
-        </Table>
-      </TableContainer>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={optimalRegionTimeMap(result)}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          columnBuffer={6}
+        />
+      </div>
     )
   }
 
@@ -108,12 +139,18 @@ const ForecastCard: FunctionComponent<any> = ({ data }): ReactElement => {
           </Typography>
           <Typography className={classes.posOne}></Typography>
         </CardContent>
-        {loading ? (
-          <LoadingMessage
-            message={'Loading cloud data. This may take a while...'}
-          />
+        {error != null ? (
+          <ErrorPage errorMessage={'Error loading cloud data'} />
         ) : (
-          renderTable()
+          <>
+            {loading ? (
+              <LoadingMessage
+                message={'Loading cloud data. This may take a while...'}
+              />
+            ) : (
+              renderTable()
+            )}
+          </>
         )}
       </React.Fragment>
     </DashboardCard>
