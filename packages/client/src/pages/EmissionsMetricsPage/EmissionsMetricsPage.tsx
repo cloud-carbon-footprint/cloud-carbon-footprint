726c9f21314046ecbfbc3c2ee9247a5ee1e5a33d
Fara Akhatova <faraakhatova@Faras-MacBook-Pro-2.local>
2022-12-19 11:36:17 -0700

    Remove emissions data rdequest and date range from component and pass it as props from App.tsx

--- packages/client/src/pages/EmissionsMetricsPage/EmissionsMetricsPage.tsx
+++ packages/client/src/pages/EmissionsMetricsPage/EmissionsMetricsPage.tsx
@@ -4,7 +4,6 @@
 
 import React, { ReactElement } from 'react'
 import { Grid } from '@material-ui/core'
-import moment, { unitOfTime } from 'moment'
 import LoadingMessage from '../../common/LoadingMessage'
 import EmissionsFilterBar from './EmissionsFilterBar'
 import CarbonIntensityMap from './CarbonIntensityMap'
@@ -13,49 +12,21 @@ import EmissionsBreakdownCard from './EmissionsBreakdownCard'
 import EmissionsOverTimeCard from './EmissionsOverTimeCard'
 import useStyles from './emissionsMetricsStyles'
 import EmissionsSidePanel from './EmissionsSidePanel/EmissionsSidePanel'
-import { useFootprintData } from '../../utils/hooks'
 import { ClientConfig } from '../../Config'
 import loadConfig from '../../ConfigLoader'
 
 interface EmissionsMetricsPageProps {
   config?: ClientConfig
   onApiError?: (e: Error) => void
+  footprint: any
 }
 
 export default function EmissionsMetricsPage({
   config = loadConfig(),
   onApiError,
+  footprint,
 }: EmissionsMetricsPageProps): ReactElement<EmissionsMetricsPageProps> {
   const classes = useStyles()
-  const dateRangeType: string = config.DATE_RANGE.TYPE
-  const dateRangeValue: string = config.DATE_RANGE.VALUE
-  let endDate: moment.Moment = moment
-    .utc()
-    .subtract(config.MINIMAL_DATE_AGE, 'days')
-  if (config.END_DATE) {
-    endDate = moment.utc(config.END_DATE)
-  }
-
-  let startDate: moment.Moment
-  if (config.PREVIOUS_YEAR_OF_USAGE) {
-    startDate = moment.utc(Date.UTC(endDate.year() - 1, 0, 1, 0, 0, 0, 0))
-  } else if (config.START_DATE) {
-    startDate = moment.utc(config.START_DATE)
-  } else {
-    startDate = moment
-      .utc()
-      .subtract(dateRangeValue, dateRangeType as unitOfTime.DurationConstructor)
-  }
-
-  const footprint = useFootprintData({
-    baseUrl: config.BASE_URL,
-    startDate,
-    endDate,
-    onApiError,
-    groupBy: config.GROUP_BY,
-    limit: parseInt(config.PAGE_LIMIT as unknown as string),
-    ignoreCache: config.DISABLE_CACHE,
-  })
 
   if (footprint.loading) {
     return (
