20446356736ff452de970bc96e32c95430930cb2
Arik Smith <ams382@case.edu>
2023-03-01 12:30:25 -0500

    [#1066] implements mongodb config option for guided install

--- packages/cli/src/GuidedInstall/gcp.ts
+++ packages/cli/src/GuidedInstall/gcp.ts
@@ -10,12 +10,12 @@ export async function GCPSetup(): Promise<EnvConfig> {
   env.GCP_USE_CARBON_FREE_ENERGY_PERCENTAGE = 'true'
 
   await confirmPrompt(
-    'Ensure you have a GCP Service Account with the permission to start BigQuery jobs and read Bigquery job results. Learn more about GCP Service Accounts [here](https://cloud.google.com/docs/authentication/getting-started).',
+    'Ensure you have a GCP Service Account with the permission to start BigQuery jobs and read BigQuery job results. Learn more about GCP Service Accounts [here](https://cloud.google.com/docs/authentication/getting-started).',
   )
 
   // does this have to be done separately or can it be part of the .env?
   await confirmPrompt(
-    'Create and download a JSON private file for this Service Account to your local filesystem. Learn more about this authentication method [here](https://cloud.google.com/docs/authentication/getting-started)',
+    'Create and download a JSON private file for this Service Account to your local filesystem. Learn more about this authentication method at (https://cloud.google.com/docs/authentication/getting-started)',
   )
 
   env.GOOGLE_APPLICATION_CREDENTIALS = await inputPrompt(
@@ -23,7 +23,7 @@ export async function GCPSetup(): Promise<EnvConfig> {
   )
 
   await confirmPrompt(
-    'Set up Google Cloud billing data to export to BigQuery. You can find the instructions for this [here](https://cloud.google.com/billing/docs/how-to/export-data-bigquery).',
+    'Set up Google Cloud billing data to export to BigQuery. You can find the instructions for this at https://cloud.google.com/billing/docs/how-to/export-data-bigquery.',
   )
 
   env.GCP_BILLING_PROJECT_ID = await inputPrompt('Enter GCP Project ID:')
