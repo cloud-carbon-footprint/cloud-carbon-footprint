78835f5ff03bd18816fe115cb904c0bc90ca6763
Arik Smith <ams382@case.edu>
2023-03-01 15:05:29 -0500

    [#1066] updates create app templates

--- packages/create-app/templates/default-app/packages/cli/src/GuidedInstall/runner.ts
+++ packages/create-app/templates/default-app/packages/cli/src/GuidedInstall/runner.ts
@@ -7,11 +7,13 @@ import { log } from '../common/external'
 import { AWSSetup } from './aws'
 import { GCPSetup } from './gcp'
 import { AzureSetup } from './azure'
+import { MongoDBSetup } from './mongo'
 
-type ProvidersConfig = {
+type ConfigOptions = {
   [key: string]: { setup: () => Promise<EnvConfig>; docs: string }
 }
-const providers: ProvidersConfig = {
+
+const configOptions: ConfigOptions = {
   AWS: {
     setup: AWSSetup,
     docs: '/docs/aws',
@@ -24,27 +26,33 @@ const providers: ProvidersConfig = {
     setup: AzureSetup,
     docs: '/docs/azure',
   },
+  MongoDB: {
+    setup: MongoDBSetup,
+    docs: 'docs/data-persistence-and-caching#mongodb-storage',
+  },
 }
 
 export async function GuidedInstall(): Promise<void> {
   log(
-    `Please follow the following prompts to configure Cloud Carbon Footprint to connect to a cloud provider. To connect to multiple cloud providers, please repeat this script for each additional provider. For an understanding of the default configurations and other configuration options, please visit the docs on the microsite: ${microsite}/docs/introduction`,
+    `Please select an option below to begin guided setup for your Cloud Carbon Footprint app. To complete multiple configuration options (i.e. setup multiple cloud providers), please repeat this script for each additional option. For an understanding of the default configurations and other setup options, please visit the docs on the microsite: ${microsite}/docs/introduction`,
   )
 
-  const provider: string = await listPrompt(
-    'Choose a cloud provider to configure:',
-    Object.keys(providers),
+  const selectedOption: string = await listPrompt(
+    'Which would you like to configure:',
+    Object.keys(configOptions),
   )
 
-  const { setup, docs } = providers[provider]
-  log(`${provider} setup. See ${microsite}${docs} for additional details.`)
+  const { setup, docs } = configOptions[selectedOption]
+  log(
+    `${selectedOption} setup. See ${microsite}${docs} for additional details.`,
+  )
   const env = await setup()
 
   await createEnvFile('./', env)
   await createEnvFile('../api/', env)
 
   log(
-    `Your Cloud Carbon Footprint application is now configured to use your cloud provider data. You can see these settings in the .env files listed above. To connect to an additional cloud provider or update an existing one, please re-run this script. For questions or more configuration options, please visit ${microsite}/docs/introduction`,
+    `Your Cloud Carbon Footprint application's configuration has been updated. You can view these settings in the .env files listed above. To complete additional setup (i.e. add an additional cloud provider), please re-run this script. For questions or more configuration options, please visit ${microsite}/docs/introduction`,
   )
   log(
     `Please make sure your appConfig object in the client Config file (packages/client/src/Config.ts) includes your cloud provider(s) in the CURRENT_PROVIDERS array`,
