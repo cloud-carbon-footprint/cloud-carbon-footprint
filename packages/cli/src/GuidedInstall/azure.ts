3375b51cba7023c5b683fd9e87e643afee07958f
Arik Smith <ams382@case.edu>
2023-03-01 14:58:11 -0500

    removes hyperlink markup from prompts

--- packages/cli/src/GuidedInstall/azure.ts
+++ packages/cli/src/GuidedInstall/azure.ts
@@ -10,7 +10,7 @@ export async function AzureSetup(): Promise<EnvConfig> {
   env.AZURE_USE_BILLING_DATA = 'true'
 
   await confirmPrompt(
-    'Register a new Azure Application via your Azure Portal, under “App Registrations”.\n\t- You do not need to set a Redirect URI or configure platform Settings. Learn more about how to do this [here](https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-register-app)',
+    'Register a new Azure Application via your Azure Portal, under “App Registrations”.\n\t- You do not need to set a Redirect URI or configure platform Settings. Learn more about how to do this here: https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-register-app',
   )
 
   await confirmPrompt(
