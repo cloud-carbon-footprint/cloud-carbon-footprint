be67a10eee02e822e501d637933178b47d1d481e
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2023-03-27 15:11:08 +0530

    add logic to update co2e value in footprint

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 //CO2e -> kilowattHours * emissions[region];
-function updateData() {
+function updateCO2eData() {
     const data = fs.readFileSync(
         path.resolve(__dirname, `../packages/client/stub-server/mockData.json`),
         'utf8',
@@ -12,9 +12,20 @@ function updateData() {
         serviceEstimates.forEach((data) => {
             let emissions = mockData.emissions.find(({ region }) => region === data.region);
             let co2e = data.kilowattHours * emissions.mtPerKwHour;
-            console.log(data.co2e, " -> ", co2e);
+            data.co2e = co2e;
+            // console.log(data, " -> ", co2e);
         })
     })
+    fs.writeFileSync(
+        path.resolve(__dirname, `../packages/client/stub-server/mockData.json`),
+        JSON.stringify(mockData),
+        (err) => {
+            if (err) {
+                console.error(err)
+            }
+        },
+    )
+    console.log(mockData);
 }
 
-updateData();
+updateCO2eData();
