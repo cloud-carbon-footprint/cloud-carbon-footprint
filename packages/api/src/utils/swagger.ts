6699a64a0a2000d906f0cccc19a013c7b166771e
Arik Smith <ams382@case.edu>
2023-06-21 16:33:01 -0400

    removes unnecessary json module config and swagger import

--- packages/api/src/utils/swagger.ts
+++ packages/api/src/utils/swagger.ts
@@ -1,7 +1,6 @@
 import { Express, Request, Response } from 'express'
 import swaggerJsdoc from 'swagger-jsdoc'
 import swaggerUi from 'swagger-ui-express'
-// import {version} from '../package.json'
 import { Logger } from '@cloud-carbon-footprint/common'
 const serverLogger = new Logger('server')
 
