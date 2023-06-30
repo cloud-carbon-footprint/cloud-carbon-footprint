7fc2fe86617336a97a019fd51c5a6420193fb983
Cam Casher <camcash714@gmail.com>
2023-06-30 13:04:26 -0600

    fix: update client imports

--- packages/client/src/utils/helpers/handleDates.ts
+++ packages/client/src/utils/helpers/handleDates.ts
@@ -1,6 +1,6 @@
 import moment, { Moment, unitOfTime } from 'moment'
-import { ClientConfig } from 'src/Config'
-import loadConfig from 'src/ConfigLoader'
+import { ClientConfig } from '../../Config'
+import loadConfig from '../../ConfigLoader'
 import { ForecastDetails } from '../../pages/RecommendationsPage/RecommendationsTable/Forecast/Forecast'
 
 interface dateProps {
