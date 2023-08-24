3b71a0d4a2b5c8eac91298905fc05cdd6a068ccd
Arik Smith <arik.m.smith@gmail.com>
2023-08-24 18:59:18 -0400

    removes double brackets from shell script conditionals for better environment compatibility

--- scripts/copyright_check.sh
+++ scripts/copyright_check.sh
@@ -7,15 +7,15 @@ copyright="© 202[1-3] Thoughtworks, Inc."
 errorCode=0
 domain=`git config user.email`
 
-if [[ "$domain" != *"thoughtworks.com"* ]]; then echo "Thank you for contributing!";
+if [ "$domain" != *"thoughtworks.com"* ]; then echo "Thank you for contributing!";
 else
   git diff --cached --name-status | while read x file; do
     if [ "$x" = 'D' ]; then continue; fi
     if [ ${x::1} = 'R' ]; then continue; fi
-    if [[ $file = *"CHANGELOG"* ]]; then continue; fi
-    if [[ $file = *".changeset"* ]]; then continue; fi
-    if [[ $file = *"microsite/"* ]]; then continue; fi
-    if [[ $file = *".adr/"* ]]; then continue; fi
+    if [ $file = *"CHANGELOG"* ]; then continue; fi
+    if [ $file = *".changeset"* ]; then continue; fi
+    if [ $file = *"microsite/"* ]; then continue; fi
+    if [ $file = *".adr/"* ]; then continue; fi
     holder=$(echo $file | grep -E "^.*\.(ts|tsx|js|jsx|md|sh)$" | wc -l)
     if [ $holder -eq 0 ]; then continue; fi
     if grep -E "$copyright" $file; then continue; fi
