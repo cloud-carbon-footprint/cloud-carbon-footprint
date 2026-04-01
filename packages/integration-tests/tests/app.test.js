caeea80516882daf5776d7d75867f3a351854387
jyothirmai.m <jyothirmai.m@thoughtworks.com>
2026-04-01 18:44:00 +0530

    GGRCCF-50: Enable integration tests for app main page

--- packages/integration-tests/tests/app.test.js
+++ packages/integration-tests/tests/app.test.js
@@ -18,11 +18,11 @@ fixture`Cloud Carbon Footprint`.page`http://127.0.0.1:3000/`
     await t.expect(page.header.exists).ok()
   })
 
-test.skip('loading screen appears when app is starting', async (t) => {
+test('loading screen appears when app is starting', async (t) => {
   await page.loadingScreen
 })
 
-test.skip('main components render with correct data when app loads', async (t) => {
+test('main components render with correct data when app loads', async (t) => {
   await t
     .expect(page.cloudProviders.with({ visibilityCheck: true }).exists)
     .ok() //this one was the problem once
@@ -37,18 +37,18 @@ test.skip('main components render with correct data when app loads', async (t) =
     .ok()
 }) //waiting seems to help, try updating wait
 
-test.skip('side drawer opens and closes when clicked', async (t) => {
+test('side drawer opens and closes when clicked', async (t) => {
   await t.click(page.drawerOpenButton).expect(page.drawerOpen.exists).ok()
   await t.click(page.drawerCloseButton).expect(page.drawerOpen.exists).notOk()
 })
 
-test.skip('total metric tons is loaded correctly with different dropdown selections', async (t) => {
+test('total metric tons is loaded correctly with different dropdown selections', async (t) => {
   await page.totalCo2Amount.with({ visibilityCheck: true }).exists //await core element before getting any of its text-specific versions
   //check initial value then check after each filter option
   await t.expect(page.totalCo2Amount.withText('metric tons CO2e').exists).ok() //the other problem element
 })
 
-test.skip('carbon equivalency component displays each option when clicked', async (t) => {
+test('carbon equivalency component displays each option when clicked', async (t) => {
   await t
     .click(page.flightsButton)
     .expect(
@@ -68,7 +68,7 @@ test.skip('carbon equivalency component displays each option when clicked', asyn
     .ok()
 })
 
-test.skip('emissions breakdown component displays each bar chart when selected', async (t) => {
+test('emissions breakdown component displays each bar chart when selected', async (t) => {
   // Maximize the window in order for all DOM elements to be visible.
   // For some reason this stops this test failing, and can help with debugging.
   // In headless mode, issues have been noted that can be resolved by resizing window: https://github.com/DevExpress/testcafe/issues/6739
@@ -90,7 +90,7 @@ test.skip('emissions breakdown component displays each bar chart when selected',
   await t.expect(page.selected.exists).ok()
 })
 
-test.skip('line chart displays the y-axis data when legend is clicked', async (t) => {
+test('line chart displays the y-axis data when legend is clicked', async (t) => {
   await t.click(page.kwhLegend).expect(page.kwhAxis.exists).ok()
   await t.click(page.costLegend).expect(page.costAxis.exists).ok()
   await t.click(page.co2eLegend).expect(page.co2eAxis.exists).notOk()
